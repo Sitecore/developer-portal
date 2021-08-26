98bce144f2841a3ac898898677ef424c1f44ed3e
Rob Earlam <robert.earlam@gmail.com>
2021-08-26 11:30:25 +1000

    Fixed paths on __archive pages

--- pages/__archive/personalization-testing/cdp.js
+++ pages/__archive/personalization-testing/cdp.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import styles from '../../styles/Home.module.css'
+import styles from '../../../styles/Home.module.css'
 import { getMarkdownData } from "../../../lib/getMarkdownData";
 import ReactMarkdown from "react-markdown";
 
