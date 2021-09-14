72760a6ddec6abf78cd50540de15ff7718505ff7
Brian George <bgeorge@horizontal.com>
2021-09-14 09:23:36 -0700

    Updated per UX recco, allowed fullscreen.

--- components/integrations/youtube/YouTubeFeed.tsx
+++ components/integrations/youtube/YouTubeFeed.tsx
@@ -1,5 +1,4 @@
 import { classnames } from '@/tailwindcss-classnames';
-import Image from 'next/image';
 import type { YouTubeVideo } from '@/interfaces/integrations';
 
 type YouTubeFeedProps = {
@@ -12,20 +11,16 @@ const YouTubeFeed = ({ content }: YouTubeFeedProps): JSX.Element => {
   }
 
   return (
-    <div>
+    <div className="my-16 max-w-screen-xl mx-auto">
       <p>YouTube</p>
       <ul className={classnames('grid', 'md:grid-cols-3', 'gap-6')}>
         {content.map(({ snippet, id }) => (
           <li key={id}>
-            {/* <Image
-              src={snippet.thumbnails.medium.url}
-              height={snippet.thumbnails.medium.height}
-              width={snippet.thumbnails.medium.width}
-            /> */}
             <div className={classnames('aspect-w-16', 'aspect-h-9')}>
               <iframe
                 className={classnames('w-full')}
                 src={`http://www.youtube.com/embed/${snippet.resourceId.videoId}?enablejsapi=1`}
+                allowFullScreen
               ></iframe>
             </div>
             <p>{snippet.title}</p>
