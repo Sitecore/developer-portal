70b7234d52f7ad684036e44070ae8c72b095714d
Anders Hopkins <4251713+andershopkins@users.noreply.github.com>
2021-09-15 06:30:28 -0500

    Added GTM/GA snippets to the app and appropriate env variable support, exposed to CS.

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@
  */
 
 const nextConfig = {
+  // Needed to expose in clientside.
+  env: {
+    GOOGLE_ANALYTICS_ID: process.env.GOOGLE_ANALYTICS_ID,
+  },
   images: {
     domains: ['sitecorecdn.azureedge.net', 'i.ytimg.com'],
   },
