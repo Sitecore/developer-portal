4b2b890c44f684c9435518f60a08d58897a751f7
Brian George <bgeorge@horizontal.com>
2021-09-20 12:36:57 -0700

    Added next image support in markdown content.

--- components/helper/MarkdownContent.tsx
+++ components/helper/MarkdownContent.tsx
@@ -2,6 +2,7 @@
 import { classnames } from '@/tailwindcss-classnames';
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
+import Image from 'next/image';
 // Interfaces
 import type { PartialData } from '@/interfaces/page-info';
 // Lib
@@ -13,6 +14,41 @@ type MarkdownContentProps = {
   hasGrid?: boolean;
 };
 
+type NodeChild = {
+  tagName: string;
+  properties: {
+    alt?: string;
+    src?: string;
+  };
+};
+
+type ParagraphNode = {
+  node: {
+    children: NodeChild[];
+  };
+  children: any[];
+};
+
+const MarkdownComponents: object = {
+  p: (paragraph: ParagraphNode) => {
+    const { node } = paragraph;
+
+    if (node.children[0].tagName === 'img') {
+      const image = node.children[0];
+      const alt = image.properties.alt;
+
+      return (
+        <div
+          className={classnames('inline-block', 'relative', 'aspect-w-16', 'aspect-h-9', 'w-full')}
+        >
+          <Image src={image.properties.src || ''} alt={alt} layout="fill" />
+        </div>
+      );
+    }
+    return <p>{paragraph.children}</p>;
+  },
+};
+
 const MarkdownContent = ({ partials, hasGrid = false }: MarkdownContentProps): JSX.Element => {
   if (hasGrid) {
     return (
@@ -22,7 +58,12 @@ const MarkdownContent = ({ partials, hasGrid = false }: MarkdownContentProps): J
             className={classnames('prose', 'p-8', 'border', 'border-gray-light', 'bg-white')}
             key={i}
           >
-            <ReactMarkdown remarkPlugins={[setHeadingIds, remarkGfm]}>{item}</ReactMarkdown>
+            <ReactMarkdown
+              remarkPlugins={[setHeadingIds, remarkGfm]}
+              components={MarkdownComponents}
+            >
+              {item}
+            </ReactMarkdown>
           </div>
         ))}
       </div>
@@ -33,7 +74,9 @@ const MarkdownContent = ({ partials, hasGrid = false }: MarkdownContentProps): J
     <VerticalGroup>
       {partials.content.map((item, i) => (
         <div className={classnames('prose')} key={i}>
-          <ReactMarkdown remarkPlugins={[setHeadingIds, remarkGfm]}>{item}</ReactMarkdown>
+          <ReactMarkdown remarkPlugins={[setHeadingIds, remarkGfm]} components={MarkdownComponents}>
+            {item}
+          </ReactMarkdown>
         </div>
       ))}
     </VerticalGroup>
