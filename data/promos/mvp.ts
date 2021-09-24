0878f186e054b7290d57e01d74d6c65d6ad52e25
Jason St-Cyr <jst-cyr@users.noreply.github.com>
2021-09-24 13:34:28 -0400

    Updated MVP promo to use DAM image

--- data/promos/mvp.ts
+++ data/promos/mvp.ts
@@ -5,7 +5,7 @@ const data: PromoCardProps = {
   description:
     'Celebrate the most active Sitecore Community members from around the world who provide valuable expertise to the Sitecore ecosystem. Do you have what it takes to be a Sitecore MVP?',
   img: {
-    src: '/images/heros/hero-example.jpeg',
+    src: 'https://mss-p-006-delivery.sitecorecontenthub.cloud/api/public/content/a3c5df7b1d06426ca0b7cac57e4804e7?v=dbd864c9',
   },
   link: {
     href: 'https://mvp.sitecore.com/',
