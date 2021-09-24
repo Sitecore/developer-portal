f4ca3518c15ef420fafa6c29b9f92684d0d941bc
Jason St-Cyr <jst-cyr@users.noreply.github.com>
2021-09-24 13:18:20 -0400

    Updating Sitecore Support promo to use DAM image

--- data/promos/sitecore-support.ts
+++ data/promos/sitecore-support.ts
@@ -5,7 +5,7 @@ const data: PromoCardProps = {
   description:
     'Access the Sitecore Support and Self-Service Portal to search the knowledge base, report an issue, and check on service status.',
   img: {
-    src: '/images/heros/hero-example.jpeg',
+    src: 'https://mss-p-006-delivery.sitecorecontenthub.cloud/api/public/content/e3e23f39681742a2ad41c9a2917c4c6e?v=1ff6d55b',
   },
   link: {
     href: 'https://support.sitecore.com',
