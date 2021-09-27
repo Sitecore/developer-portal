91e17f1645f057a46df9855c56aeb2a1bbceed68
Jason St-Cyr <jst-cyr@users.noreply.github.com>
2021-09-27 09:35:17 -0400

    Adding some copy to the community callout boxes

--- data/data-community-list.ts
+++ data/data-community-list.ts
@@ -12,27 +12,27 @@ type CommunityListItem = {
 const content: CommunityListItem[] = [
   {
     title: 'Slack',
-    description: 'Talk to the community on Slack.',
+    description: 'Talk to the community on this community-run Slack. Share, connect, and help each other out!',
     href: 'https://sitecore.chat',
-    linkText: 'Slack',
+    linkText: 'Join the conversation',
     img: {
       src: 'https://mss-p-006-delivery.sitecorecontenthub.cloud/api/public/content/757e3faaa19f41da9c47aac876e909dd?v=16c38e3f',
     },
   },
   {
     title: 'Stack Exchange',
-    description: 'Ask questions on Sitecore Stack Exchange.',
+    description: 'Ask and answer questions on the community-driven Sitecore Stack Exchange site.',
     href: 'https://sitecore.stackexchange.com',
-    linkText: 'Stack Exchange',
+    linkText: 'Ask a question',
     img: {
       src: 'https://mss-p-006-delivery.sitecorecontenthub.cloud/api/public/content/f3357193e74148bb82ff46a4cc888e81?v=e21b62a0',
     },
   },
   {
     title: 'Forums',
-    description: 'Connect with others in the forums.',
+    description: 'Connect with other members of the community in the official Sitecore community forums.',
     href: 'https://community.sitecore.net/',
-    linkText: 'Community forums',
+    linkText: 'Start a discussion',
     img: {
       src: 'https://mss-p-006-delivery.sitecorecontenthub.cloud/api/public/content/80dbfc3c6ecc42bdaa46d4779562e528?v=7784264f',
     },
