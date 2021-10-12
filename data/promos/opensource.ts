5d153d7c6088e991d203f2ba5b5bffda9698d21a
Mark van Aalst <markvanaalst@gmail.com>
2021-10-12 16:43:51 +0200

    Open-source promo

---
+++ data/promos/opensource.ts
@@ -0,0 +1,16 @@
+import type { PromoCardProps } from '@/components/cards/PromoCard';
+
+const data: PromoCardProps = {
+  title: 'Open Source @ Sitecore',
+  description:
+    'Did you know that Sitecore offers a lot of open source initiatives? ',
+  img: {
+    src: 'https://mss-p-006-delivery.sitecorecontenthub.cloud/api/public/content/a3c5df7b1d06426ca0b7cac57e4804e7?v=dbd864c9',
+  },
+  link: {
+    href: 'https://github.com/Sitecore',
+    text: 'Check out Sitecore @ GitHub',
+  },
+};
+
+export default data;
