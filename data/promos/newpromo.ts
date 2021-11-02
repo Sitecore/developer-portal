51ff3a7f5a180563a2247e03027d746029f30a5d
Mark van Aalst <markvanaalst@gmail.com>
2021-11-02 16:12:13 +0100

    Adding scaffolding for new promo

---
+++ data/promos/newpromo.ts
@@ -0,0 +1,15 @@
+import type { PromoCardProps } from '@/components/cards/PromoCard';
+
+const data: PromoCardProps = {
+  title: 'Promo title can be put here',
+  description: 'Please add a description that provides a bit more context ',
+  img: {
+    src: 'https://mss-p-006-delivery.sitecorecontenthub.cloud/api/public/content/a3c5df7b1d06426ca0b7cac57e4804e7?v=dbd864c9',
+  },
+  link: {
+    href: '#',
+    text: 'Read more link',
+  },
+};
+
+export default data;
