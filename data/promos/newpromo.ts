694cba8aef0e9553f565e1822d4da272cd48d068
Jason St-Cyr <jst-cyr@users.noreply.github.com>
2021-11-02 13:17:19 -0400

    Updating home page promo for title, description, and imagery.
    Still needs link to announcement.

--- data/promos/newpromo.ts
+++ data/promos/newpromo.ts
@@ -1,14 +1,14 @@
 import type { PromoCardProps } from '@/components/cards/PromoCard';
 
 const data: PromoCardProps = {
-  title: 'Promo title can be put here',
-  description: 'Please add a description that provides a bit more context ',
+  title: 'Sitecore XP 10.2 available now!',
+  description: 'Important changes like Static Site Generation with MVC/SXA sites and headless support in Horizon. You can download the latest release now!',
   img: {
-    src: 'https://mss-p-006-delivery.sitecorecontenthub.cloud/api/public/content/a3c5df7b1d06426ca0b7cac57e4804e7?v=dbd864c9',
+    src: 'https://mss-p-006-delivery.sitecorecontenthub.cloud/api/public/content/2f56b775bd304f2eae0ea219f4de0ab6?v=f51283ad',
   },
   link: {
     href: '#',
-    text: 'Read more link',
+    text: 'Learn more about the release...',
   },
 };
 
