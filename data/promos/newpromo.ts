0922b26bf1d8603833e7fc4480f01bf10ab4c1f5
Mark van Aalst <markvanaalst@gmail.com>
2021-11-15 12:24:55 +0100

    updated content based on feedback Tamas

--- data/promos/newpromo.ts
+++ data/promos/newpromo.ts
@@ -3,7 +3,7 @@ import type { PromoCardProps } from '@/components/cards/PromoCard';
 const data: PromoCardProps = {
   title: 'Sitecore Developer Experience Survey 2021 !',
   description:
-    'We want to hear from you! Take the Developer Experience Survey if you’ve worked professionally with the Sitecore platform or contributed to a project in the past 12 months. Your answers will help shape our future.!',
+    'We want to hear from you! Take the Developer Experience Survey if you’ve worked professionally with the Sitecore platform or contributed to a project in the past 12 months. Your answers will help shape our future! Just provide your name and email at the end of the survey to be part of the raffle.',
   img: {
     src: 'https://mss-p-006-delivery.sitecorecontenthub.cloud/api/public/content/be4a712f6d7149339b599ac33721bbeb?v=cbd8fad3',
   },
