386422a681765676e75900c8387fb6f14ec1c2c4
Mark van Aalst <markvanaalst@gmail.com>
2021-11-22 11:44:26 +0100

    added security headers

--- next.config.js
+++ next.config.js
@@ -21,6 +21,20 @@ const nextConfig = {
     domains: ['sitecorecdn.azureedge.net', 'i.ytimg.com', 'mss-p-006-delivery.sitecorecontenthub.cloud'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920],
   },
+  "routes": [
+    {
+      "src": "/.*",
+      "headers": {
+        "Content-Security-Policy": "default-src 'self' data: blob: *.sitecore.com *.sitecore.net *.stylelabs.cloud *.googleapis.com *.gstatic.com *.azureedge.net; frame-src * 'self' 'unsafe-inline'; frame-ancestors 'self' https://*.sitecore.com; script-src * blob: data: 'self' 'unsafe-inline' 'unsafe-eval'; script-src-elem * 'self' 'unsafe-inline'; script-src-attr * 'self' 'unsafe-inline'; style-src * 'self' 'unsafe-inline'; style-src-elem * 'self' 'unsafe-inline'; style-src-attr * 'self' 'unsafe-inline' data:; img-src * 'self' 'unsafe-inline' data:; font-src * data: 'self' 'unsafe-inline'; connect-src *; object-src 'none'; media-src * data: blob: 'unsafe-inline' 'unsafe-eval';",
+        "Referrer-Policy": "same-origin",
+        "X-Content-Type-Options": "nosniff",
+        "X-Frame-Options": "deny",
+        "Referrer-Policy": "same-origin",
+        "X-XSS-Protection": "1; mode=block"
+      },
+      "continue": true
+    }
+  ]
 };
 
 module.exports = withTM(nextConfig);
