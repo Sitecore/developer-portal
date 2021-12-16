1619c5e85ae16029cf79a88c7fecca5779cbd43f
Jason St-Cyr <jst-cyr@users.noreply.github.com>
2021-12-16 10:49:18 -0500

    Moving post from Blog to News (new URL)

--- data/promos/newpromo.ts
+++ data/promos/newpromo.ts
@@ -8,7 +8,7 @@ const data: PromoCardProps = {
     src: 'https://mss-p-006-delivery.stylelabs.cloud/api/public/content/a3c5df7b1d06426ca0b7cac57e4804e7?v=dbd864c9',
   },
   link: {
-    href: 'https://community.sitecore.com/community?id=community_blog&sys_id=fffb00b31b580110b8954371b24bcbfb',
+    href: 'https://community.sitecore.com/community?id=community_blog&sys_id=002249581ba40110722d4042b24bcb4c',
     text: 'Read more about the news!',
   },
 };
