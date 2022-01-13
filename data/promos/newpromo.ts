d60082e369448bd74d2f8f3a4d8d213b27814d7e
Jason St-Cyr <jst-cyr@users.noreply.github.com>
2022-01-13 12:12:05 -0500

    Changed URL to HTTPS to avoid 'secure' issues in browsers and added a hyphen to text.

--- data/promos/newpromo.ts
+++ data/promos/newpromo.ts
@@ -3,12 +3,12 @@ import type { PromoCardProps } from '@/components/cards/PromoCard';
 const data: PromoCardProps = {
   title: 'Sitecore Hackathon 2022',
   description:
-    'The 2022 Sitecore Hackathon is a free online community driven event organized by Akshay Sura and supported by Sitecore. Make sure you sign up your team by 6 pm EST February 18, 2022 to have the opportunity to participate in a contest. Visit the Hackathon website for more information on this event',
+    'The 2022 Sitecore Hackathon is a free online community-driven event organized by Akshay Sura and supported by Sitecore. Make sure you sign up your team by 6 pm EST February 18, 2022 to have the opportunity to participate in a contest. Visit the Hackathon website for more information on this event',
   img: {
     src: 'https://mss-p-006-delivery.stylelabs.cloud/api/public/content/2f9717ea009a49f381e1c63270e37896?v=7c6d04bd',
   },
   link: {
-    href: 'http://www.sitecorehackathon.org/sitecore-hackathon-2022/',
+    href: 'https://www.sitecorehackathon.org/sitecore-hackathon-2022/',
     text: 'Sitecore Hackathon website',
   },
 };
