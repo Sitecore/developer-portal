4a10ec82a22c651717447f58ba4333ab9fc6185e
Mark van Aalst <markvanaalst@gmail.com>
2022-01-22 01:01:34 +0100

    Hide edit link when no markdown is used

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -114,23 +114,25 @@ const Layout = ({ pageInfo, children }: LayoutProps): JSX.Element => {
         {children}
       </main>
       <Footer>
-        <button className="text-xs mt-4 mr-0 flex items-center hover:text-white">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-5 w-5 fill-current"
-            viewBox="0 0 20 20"
-          >
-            <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z" />
-            <path
-              fillRule="evenodd"
-              d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z"
-              clipRule="evenodd"
-            />
-          </svg>
-          <a className="pl-1" href={`${pageInfo.fileName}`}>
-            Suggest an edit
-          </a>
-        </button>
+        {pageInfo.fileName && (
+          <button className="border border-white text-white text-xs mt-4 rounded-sm py-1 w-40 px-4 mr-0 flex items-center hover:bg-teal hover:text-white">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5 fill-current"
+              viewBox="0 0 20 20"
+            >
+              <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z" />
+              <path
+                fillRule="evenodd"
+                d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z"
+                clipRule="evenodd"
+              />
+            </svg>
+            <a className="pl-1" href={`${pageInfo.fileName}`}>
+              Suggest an edit
+            </a>
+          </button>
+        )}
       </Footer>
     </div>
   );
