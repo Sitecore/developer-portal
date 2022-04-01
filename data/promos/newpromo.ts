4bdebc5e212c148ed99e0579970582053b6f1f8a
Mark van Aalst <markvanaalst@gmail.com>
2022-04-01 13:30:01 +0200

    Update promo

--- data/promos/newpromo.ts
+++ data/promos/newpromo.ts
@@ -3,13 +3,13 @@ import type { PromoCardProps } from '@/components/cards/PromoCard';
 const data: PromoCardProps = {
   title: 'Virtual Developer Day',
   description:
-    'Sharpen your Sitecore dev skills by registering for 24 hours of developer-led sessions and camaraderie with Sitecore’s global developer community and MVPs.',
+    "This April 13th 2022 sharpen your Sitecore dev skills by registering for 24 hours of developer-led sessions and camaraderie with Sitecore’s global developer community and MVPs. Don't miss out on technical tips, tips based on the latest technology, and much more! Register today for free!",
   img: {
-    src: 'https://mss-p-006-delivery.stylelabs.cloud/api/public/content/2e10a5a7d825482f9e23c096c79f7635?v=94325149',
+    src: 'https://mss-p-006-delivery.stylelabs.cloud/api/public/content/2e10a5a7d825482f9e23c096c79f7635?v=dd90b9b6',
   },
   link: {
     href: 'https://www.sitecore.com/company/news-events/events/2022/03/virtual-developer-day',
-    text: 'Register now for free!',
+    text: 'Join for free!',
   },
 };
 
