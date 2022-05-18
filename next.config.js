bdb429a42bed3305dfb4d523d9a54c671ce34121
Mark van Aalst <markvanaalst@gmail.com>
2022-05-18 11:24:07 +0200

    Adding previous newsletters

--- next.config.js
+++ next.config.js
@@ -62,6 +62,7 @@ const nextConfig = {
       'i.ytimg.com',
       'mss-p-006-delivery.sitecorecontenthub.cloud',
       'mss-p-006-delivery.stylelabs.cloud',
+      'go.sitecore.com'
     ],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920],
   },
