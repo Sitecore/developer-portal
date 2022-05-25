f3c872fa3f07c03545b192e274e3dcf1bba5dcda
Mark van Aalst <markvanaalst@gmail.com>
2022-05-25 13:23:26 +0200

    adding promo to introduce newsletter archive

--- data/promos/newpromo.ts
+++ data/promos/newpromo.ts
@@ -1,15 +1,14 @@
 import type { PromoCardProps } from '@/components/cards/PromoCard';
 
 const data: PromoCardProps = {
-  title: 'Virtual Developer Day',
-  description:
-    "Did you miss the Virtual Developer Day? You can watch it now on YouTube! Sharpen your Sitecore dev skills with over 8 hours of developer-led sessions from Sitecore’s global developer community and MVPs. Don't miss out on technical tips, tips based on the latest technology, and much more!",
+  title: 'Newsletter archive',
+  description: 'Read earlier editions of the Sitecore for Developers newsletter. ',
   img: {
-    src: 'https://mss-p-006-delivery.stylelabs.cloud/api/public/content/c612f3d1efbe4e0cb946ab96d0b4aea1?v=0cca3868',
+    src: 'https://mss-p-006-delivery.stylelabs.cloud/api/public/content/6fb61e46117d40b39cd0f7ba7b2e060c?v=fac3ab80',
   },
   link: {
-    href: 'https://www.youtube.com/watch?v=fkXZEYxKnGY',
-    text: 'Watch now!',
+    href: '/newsletter',
+    text: 'Check it out!',
   },
 };
 
