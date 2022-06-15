40f18850e05bc5a44607d4a802819e2c8a1e8d95
Mark van Aalst <markvanaalst@gmail.com>
2022-06-15 10:56:34 +0200

    Fix invalid svg attributes

--- components/helper/ContentPager.tsx
+++ components/helper/ContentPager.tsx
@@ -41,9 +41,9 @@ const ContentPager = ({ paging, context, root }: ContentPagerProps): JSX.Element
                   xmlns="http://www.w3.org/2000/svg"
                 >
                   <path
-                    fill-rule="evenodd"
+                    fillRule="evenodd"
                     d="M7.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l2.293 2.293a1 1 0 010 1.414z"
-                    clip-rule="evenodd"
+                    clipRule="evenodd"
                   ></path>
                 </svg>
                 <p className={classnames('ml-2')}>{paging.previous?.title}</p>
@@ -77,9 +77,9 @@ const ContentPager = ({ paging, context, root }: ContentPagerProps): JSX.Element
                   xmlns="http://www.w3.org/2000/svg"
                 >
                   <path
-                    fill-rule="evenodd"
+                    fillRule="evenodd"
                     d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
-                    clip-rule="evenodd"
+                    clipRule="evenodd"
                   ></path>
                 </svg>
               </div>
