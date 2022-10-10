09fe9a2fb0ba251c4d8be9521d0492e16e1c017d
Mark van Aalst <markvanaalst@gmail.com>
2022-10-10 10:25:06 +0200

    fix formatting date on safari

--- src/common/translate-date.ts
+++ src/common/translate-date.ts
@@ -1,6 +1,7 @@
 const translateDate = (dateString: string): string => {
   const locale = typeof navigator !== 'undefined' ? navigator.language : 'en-us';
-  const date = new Date(dateString);
+  console.log(dateString);
+  const date = new Date(dateString.replace(/-/g, '/'));
   return new Intl.DateTimeFormat(locale, {
     day: 'numeric',
     month: 'numeric',
