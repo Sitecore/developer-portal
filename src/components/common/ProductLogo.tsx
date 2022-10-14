b2059b9d32edb97602117b3eebae3615d07d972d
Mark van Aalst <markvanaalst@gmail.com>
2022-10-14 16:21:42 +0200

    Logo helper component

---
+++ src/components/common/ProductLogo.tsx
@@ -0,0 +1,27 @@
+import { GetProductLogo } from '@/src/common/assets';
+import { classnames, TTailwindString } from '@/src/common/types/tailwindcss-classnames';
+import Image from 'next/image';
+
+export type ProductLogoProps = {
+  product: string;
+  variant: string;
+  alt?: string;
+  className?: TTailwindString;
+};
+
+const ProductLogo = ({ product, variant, alt, className }: ProductLogoProps): JSX.Element => {
+  const productImage = GetProductLogo(product, variant);
+
+  return (
+    <Image
+      src={productImage}
+      alt={alt != null ? alt : ''}
+      className={classnames('relative', 'z-10', className)}
+      layout="fill"
+      objectFit="fill"
+      priority={true}
+    />
+  );
+};
+
+export default ProductLogo;
