202c005e2f1583d2397c7d8b314e185967a1033d
Mark van Aalst <markvanaalst@gmail.com>
2022-10-18 01:31:16 +0200

    fix aspect ratio

--- src/components/videoPromo/index.tsx
+++ src/components/videoPromo/index.tsx
@@ -28,7 +28,7 @@ const Video = ({ videoUrl }: VideoProps): JSX.Element => (
 
 const YouTube = ({ youTubeId }: VideoProps): JSX.Element => {
   return (
-    <div className={classnames('aspect-h-9', 'aspect-w-16', 'w-full')}>
+    <div className={classnames('flex', 'flex-col', 'justify-center', 'w-full')}>
       <LiteYouTubeEmbed
         id={`${youTubeId}`} // Default none, id of the video or playlist
         adNetwork={false} // Default true, to preconnect or not to doubleclick addresses called by YouTube iframe (the adnetwork from Google)
@@ -38,9 +38,9 @@ const YouTube = ({ youTubeId }: VideoProps): JSX.Element => {
         poster="maxresdefault" // Defines the image size to call on first render as poster image. Possible values are "default","mqdefault",  "hqdefault", "sddefault" and "maxresdefault". Default value for this prop is "hqdefault". Please be aware that "sddefault" and "maxresdefault", high resolution images are not always avaialble for every video. See: https://stackoverflow.com/questions/2068344/how-do-i-get-a-youtube-video-thumbnail-from-the-youtube-api
         title="YouTube Embed" // a11y, always provide a title for iFrames: https://dequeuniversity.com/tips/provide-iframe-titles Help the web be accessible ;)
         noCookie={true} //Default false, connect to YouTube via the Privacy-Enhanced Mode using https://www.youtube-nocookie.com
-        wrapperClass="yt-lite bg-no-repeat bg-cover"
-        iframeClass="absolute top-0 w-full h-full"
-        playerClass="lty-playbtn absolute z-10"
+        wrapperClass="yt-lite bg-no-repeat bg-cover relative"
+        iframeClass="top-0 w-full h-full"
+        playerClass="lty-playbtn z-10"
       />
     </div>
   );
@@ -61,7 +61,7 @@ const VideoPromo = ({
       {isImageLeft && videoUrl != undefined && <Video videoUrl={videoUrl} />}
       {isImageLeft && youTubeId != undefined && <YouTube youTubeId={youTubeId} />}
       <div className={classnames('flex', 'flex-col', 'justify-center')}>
-        <h2 className={classnames('heading-md', 'mb-4')}>{title}</h2>
+        <h2 className={classnames('heading-md', 'mb-4', 'mt-0')}>{title}</h2>
         <p className={classnames('text-theme-text-alt', { 'mb-8': !!linkText })}>{description}</p>
         {!!linkText && (
           <div>
