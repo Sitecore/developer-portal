fef0b22cdb4de8d5f7cbdb30bbe750725b9dd50f
Mark van Aalst <markvanaalst@gmail.com>
2023-02-14 11:01:04 +0100

    align framework logo to the right

--- src/components/cards/Repository.tsx
+++ src/components/cards/Repository.tsx
@@ -14,10 +14,10 @@ export const Repository = ({ name, description, repositoryUrl, framework }: Repo
   return (
     <div className="flex flex-col bg-white border border-theme-border dark:bg-theme-bg-alt dark:bg-gray-800 dark:border-gray-700 p-3 shadow-md">
       <div className="p-5">
-        <div className="w-1/3">
+        <div className="w-1/3 ml-auto mr-0 opacity-75	">
           <SvgLogo logo={frameworkType} />
         </div>
-        {name && <h4>{name}</h4>}
+        {name && <h4 className="mt-2">{name}</h4>}
         {description && (
           <p className="mb-3 h-24 font-normal text-gray-700 dark:text-gray-400">{description}</p>
         )}
