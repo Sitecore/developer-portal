027b04559da30e78c27f1c6b9fe109558cbb42ab
Mark van Aalst <markvanaalst@gmail.com>
2023-02-15 09:47:56 +0100

    fix button positioning

--- src/components/common/GitHubButton.tsx
+++ src/components/common/GitHubButton.tsx
@@ -4,10 +4,13 @@ import SvgIcon from './SvgIcon';
 export type GithubButtonProps = {
   url: string;
   text: string;
+  className?: string;
 };
 
-export const GithubButton = ({ url, text }: GithubButtonProps) => (
-  <div className="text-white bg-black hover:bg-black/30 focus:ring-4 focus:outline-none focus:ring-[#24292F]/50 rounded-lg text-sm px-5 py-2 text-center inline-flex items-center dark:focus:ring-gray-500 dark:bg-teal dark:hover:bg-teal-dark mr-2 mb-2 mt-2">
+export const GithubButton = ({ url, text, className = '' }: GithubButtonProps) => (
+  <div
+    className={`text-white bg-black hover:bg-black/30 focus:ring-4 focus:outline-none focus:ring-[#24292F]/50 rounded-lg text-sm px-5 py-2 text-center inline-flex items-center dark:focus:ring-gray-500 dark:bg-teal dark:hover:bg-teal-dark ${className}`}
+  >
     <Link href={url} className="text-xs no-underline">
       <SvgIcon
         icon="github"
