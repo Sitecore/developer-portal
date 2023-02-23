16c3e364faaf0d653b846ba911e4d5044ee6ffb7
Mark van Aalst <markvanaalst@gmail.com>
2023-02-23 13:26:47 +0100

    adjusted navigation

--- apps/web/data/navigation.ts
+++ apps/web/data/navigation.ts
@@ -40,53 +40,19 @@ export const sitecoreQuickLinks: NavigationData = {
 
 export const mainNavigation: NavigationData[] = [
   {
-    title: 'Release Notes',
-    url: '/release-notes',
+    title: 'Changelog',
+    url: '/changelog',
   },
   {
     title: 'By product',
     children: [
       {
         title: 'Content Hub One',
-        url: '/release-notes/contenthub-one',
-        children: [
-          {
-            title: 'Change Type 1',
-            url: 'https://community.sitecore.com',
-            external: true,
-          },
-          {
-            title: 'Change Type 2',
-            url: 'https://community.sitecore.com/community?id=community_forum&sys_id=a1c2eb6b1b313c10486a4083b24bcbba',
-            external: true,
-          },
-          {
-            title: 'Change Type 3',
-            url: 'https://community.sitecore.com/community?id=community_forum&sys_id=7a84272f1b313c10486a4083b24bcbd5',
-            external: true,
-          },
-        ],
+        url: '/changelog/contenthub-one',
       },
       {
         title: 'CDP',
-        url: '/release-notes/cdp',
-        children: [
-          {
-            title: 'Change Type 1',
-            url: 'https://community.sitecore.com',
-            external: true,
-          },
-          {
-            title: 'Change Type 2',
-            url: 'https://community.sitecore.com/community?id=community_forum&sys_id=a1c2eb6b1b313c10486a4083b24bcbba',
-            external: true,
-          },
-          {
-            title: 'Change Type 3',
-            url: 'https://community.sitecore.com/community?id=community_forum&sys_id=7a84272f1b313c10486a4083b24bcbd5',
-            external: true,
-          },
-        ],
+        url: '/changelog/cdp',
       },
     ],
   },
