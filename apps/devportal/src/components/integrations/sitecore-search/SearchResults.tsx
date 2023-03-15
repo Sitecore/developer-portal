b2e2bab2b147473f88f08cac6d4373bb1ee801f3
Rob Earlam <robert.earlam@gmail.com>
2023-03-15 14:46:53 +1100

    Added facets out, fixed issue with sort output

--- apps/devportal/src/components/integrations/sitecore-search/SearchResults.tsx
+++ apps/devportal/src/components/integrations/sitecore-search/SearchResults.tsx
@@ -16,16 +16,13 @@ export const SearchResults = (props: SearchResultsType) => {
   const {
     actions: { onSortChange },
     context: { page = 0, itemsPerPage = initialArticlesPerPage },
-    queryResult: { isLoading, data: { sort: { choices: sortChoices = [] } = {}, total_item: totalItems = 0, content: articles = [] } = {} },
+    queryResult: { isLoading, data: { sort: { choices: sortChoices = [] } = {}, total_item: totalItems = 0, content: articles = [], facet: facets = [] } = {} },
   } = useSearchResults(() => {
     // initialization code
     return {
       itemsPerPage: initialArticlesPerPage,
       keyphrase: initialKeyphrase,
     };
-  });
-  const sortOptions = sortChoices.map(({ name, label }) => {
-    return { label, value: `${name}` };
   });
   return (
     <>
@@ -41,31 +38,51 @@ export const SearchResults = (props: SearchResultsType) => {
                   onSortChange({ sortType: event.target.value });
                 }}
               >
-                {sortOptions.map((option) => (
-                  <option key={option.value} value={option.value} />
+                {sortChoices.map((option, index) => (
+                  <option key={index} value={option.name}>
+                    {option.label}
+                  </option>
                 ))}
               </select>
             </div>
             {articles.length && <QuerySummary currentPage={page} resultsPerPage={itemsPerPage} totalResults={totalItems} title={title} />}
           </div>
 
-          <div>
-            <ul>
-              {articles.map((result, index) => (
-                <li key={index}>
-                  <div className="border-theme-border relative border-b p-4">
-                    <a href={result.url} className="group">
-                      {result.image_url && <Image width={25} height={25} src={result.image_url} alt={title} />}
-                      <p className="font-bold group-hover:underline">{result.title}</p>
-                      <span className="text-xs italic">{result.url}</span>
-                      <p>{result.description}</p>
-                      <p>{result.index_name}</p>
-                      <span className="absolute inset-0 z-10"></span>
-                    </a>
-                  </div>
-                </li>
+          <div className="flex flex-row">
+            <div className="mt-4 basis-1/4">
+              {facets.map((facet, index) => (
+                <div key={index}>
+                  <h4 className="font-bold">{facet.label}</h4>
+                  <ul>
+                    {facet.value.map((facetOption, index) => (
+                      <li key={index}>
+                        <input type="checkbox" value={facetOption.id} />
+                        {facetOption.text} ({facetOption.count})
+                      </li>
+                    ))}
+                  </ul>
+                </div>
               ))}
-            </ul>
+            </div>
+
+            <div className="basis-3/4">
+              <ul>
+                {articles.map((result, index) => (
+                  <li key={index}>
+                    <div className="border-theme-border relative border-b p-4">
+                      <a href={result.url} className="group">
+                        {result.image_url && <Image width={25} height={25} src={result.image_url} alt={title} />}
+                        <p className="font-bold group-hover:underline">{result.title}</p>
+                        <span className="text-xs italic">{result.url}</span>
+                        <p>{result.description}</p>
+                        <p>{result.index_name}</p>
+                        <span className="absolute inset-0 z-10"></span>
+                      </a>
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            </div>
           </div>
         </div>
       )}
