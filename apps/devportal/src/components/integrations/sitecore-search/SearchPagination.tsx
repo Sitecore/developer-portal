ee58eeca243d76f0aa1b2361ab0365cd2b82e4ff
Rob Earlam <robert.earlam@gmail.com>
2023-03-30 11:06:05 +1100

    Fixed issue with large number of pages, updated site name in search results

--- apps/devportal/src/components/integrations/sitecore-search/SearchPagination.tsx
+++ apps/devportal/src/components/integrations/sitecore-search/SearchPagination.tsx
@@ -12,17 +12,27 @@ export const SearchResults = (props: SearchPaginationType) => {
   const { page, defaultCurrentPage, onPageNumberChange, totalItems, pageSize } = props;
   const pageCount = totalItems < pageSize ? 1 : Math.ceil(totalItems / pageSize);
   const pages = Array.from(Array(pageCount).keys()).map((x) => x + 1);
+  const skipPages = pageCount > 10;
+  if (skipPages) {
+    pages.splice(5, 40);
+  }
+
   const activePaginationClasses = 'bg-theme-text text-theme-bg';
 
   return (
     <Pagination.Root currentPage={page} defaultCurrentPage={defaultCurrentPage} totalPages={pageCount} onPageChange={onPageNumberChange} className="mt-8 flex items-center justify-center">
+      <Pagination.PrevPage className={`border-theme-border mx-2 border py-1 px-2 hover:underline ${page == 1 ? 'hidden' : ''}`}>«</Pagination.PrevPage>
       <Pagination.Pages>
-        {pages.map((p) => (
-          <Pagination.Page key={p} aria-label={`Page ${p}`} page={p} onClick={(e) => e.preventDefault()} className={`border-theme-border mx-2 border py-1 px-2 hover:underline ${p == page ? [activePaginationClasses] : ''}`}>
-            {p}
-          </Pagination.Page>
+        {pages.map((p, index) => (
+          <>
+            <Pagination.Page key={p} aria-label={`Page ${p}`} page={p} onClick={(e) => e.preventDefault()} className={`border-theme-border mx-2 border py-1 px-2 hover:underline ${p == page ? [activePaginationClasses] : ''}`}>
+              {p}
+            </Pagination.Page>
+            {skipPages && index == 4 && <span className="ml-4 mr-4">...</span>}
+          </>
         ))}
       </Pagination.Pages>
+      <Pagination.NextPage className={`border-theme-border mx-2 border py-1 px-2 hover:underline ${page == pageCount ? 'hidden' : ''}`}>»</Pagination.NextPage>
     </Pagination.Root>
   );
 };
