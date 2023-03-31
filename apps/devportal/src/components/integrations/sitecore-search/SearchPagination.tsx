4dcb82505a880d5a057b8da03014e3c6cf3bd8e8
Rob Earlam <robert.earlam@gmail.com>
2023-03-31 15:40:18 +1100

    Added new render mode for search pagination control

--- apps/devportal/src/components/integrations/sitecore-search/SearchPagination.tsx
+++ apps/devportal/src/components/integrations/sitecore-search/SearchPagination.tsx
@@ -8,13 +8,22 @@ export interface SearchPaginationType {
   pageSize: number;
 }
 
+enum PaginationRenderType {
+  AllPages,
+  TruncateMiddle,
+  TruncateFirstLast,
+}
+
 export const SearchResults = (props: SearchPaginationType) => {
   const { page, defaultCurrentPage, onPageNumberChange, totalItems, pageSize } = props;
   const pageCount = totalItems < pageSize ? 1 : Math.ceil(totalItems / pageSize);
-  const pages = Array.from(Array(pageCount).keys()).map((x) => x + 1);
-  const skipPages = pageCount > 10;
-  if (skipPages) {
+  let pages = Array.from(Array(pageCount).keys()).map((x) => x + 1);
+  const paginationRenderType: PaginationRenderType = pageCount < 10 ? PaginationRenderType.AllPages : page <= 5 || page > pageCount - 5 ? PaginationRenderType.TruncateMiddle : PaginationRenderType.TruncateFirstLast;
+
+  if (paginationRenderType === PaginationRenderType.TruncateMiddle) {
     pages.splice(5, pageCount - 10);
+  } else if (paginationRenderType === PaginationRenderType.TruncateFirstLast) {
+    pages = pages.slice(page - 5, page + 4);
   }
 
   const activePaginationClasses = 'bg-theme-text text-theme-bg';
@@ -22,16 +31,18 @@ export const SearchResults = (props: SearchPaginationType) => {
   return (
     <Pagination.Root currentPage={page} defaultCurrentPage={defaultCurrentPage} totalPages={pageCount} onPageChange={onPageNumberChange} className="mt-8 flex items-center justify-center">
       <Pagination.PrevPage className={`border-theme-border mx-2 border py-1 px-2 hover:underline ${page == 1 ? 'hidden' : ''}`}>«</Pagination.PrevPage>
+      {paginationRenderType == PaginationRenderType.TruncateFirstLast && <span className="ml-4 mr-4">...</span>}
       <Pagination.Pages>
         {pages.map((p, index) => (
           <>
             <Pagination.Page key={p} aria-label={`Page ${p}`} page={p} onClick={(e) => e.preventDefault()} className={`border-theme-border mx-2 border py-1 px-2 hover:underline ${p == page ? [activePaginationClasses] : ''}`}>
               {p}
             </Pagination.Page>
-            {skipPages && index == 4 && <span className="ml-4 mr-4">...</span>}
+            {paginationRenderType == PaginationRenderType.TruncateMiddle && index == 4 && <span className="ml-4 mr-4">...</span>}
           </>
         ))}
       </Pagination.Pages>
+      {paginationRenderType == PaginationRenderType.TruncateFirstLast && <span className="ml-4 mr-4">...</span>}
       <Pagination.NextPage className={`border-theme-border mx-2 border py-1 px-2 hover:underline ${page == pageCount ? 'hidden' : ''}`}>»</Pagination.NextPage>
     </Pagination.Root>
   );
