d8005078abe61d547a01a3f70ba145dc2a411b79
Mark van Aalst <markvanaalst@gmail.com>
2023-04-13 15:07:54 +0200

    add dark mode for alert

--- packages/ui/components/common/Alert.tsx
+++ packages/ui/components/common/Alert.tsx
@@ -7,8 +7,8 @@ type AlertProps = {
 
 export const Alert = ({ children, icon }: AlertProps) => {
   return (
-    <div role="alert" className={`text-theme-text mt-8 inline-flex w-full rounded-lg bg-blue-100 px-4 py-3 text-xs`}>
-      <SvgIcon icon={icon} className="relative top-0.5 mr-2 h-5 w-5 text-blue-500" width={24} height={24} />
+    <div role="alert" className={`text-theme-text mt-8 inline-flex w-full rounded-lg bg-blue-100 px-4 py-3 text-xs dark:bg-blue-900`}>
+      <SvgIcon icon={icon} className="relative top-0.5 mr-2 h-5 w-5 text-blue-500 dark:text-blue-100" width={24} height={24} />
       {children}
     </div>
   );
