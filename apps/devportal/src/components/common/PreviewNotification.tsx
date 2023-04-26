6b80b2e45334a12a1355c1b9e3a16283650c8c11
Mark van Aalst <markvanaalst@gmail.com>
2023-04-26 20:03:31 +0200

    remove margin for mobile

--- apps/devportal/src/components/common/PreviewNotification.tsx
+++ apps/devportal/src/components/common/PreviewNotification.tsx
@@ -10,7 +10,7 @@ const PreviewNotification = ({ enabled, page }: PreviewNotificationProps): JSX.E
   if (enabled) {
     return (
       <div className="fixed bottom-0 z-50 flex w-full bg-orange-100 text-left dark:bg-teal-700 ">
-        <div className="mx-auto my-4 w-1/2">
+        <div className="mx-auto md:my-4 md:w-1/2">
           <Message type={Type.Alert} message="You are viewing the developer portal in preview mode">
             <SmallLinkButton text={'Exit preview'} href={`/api/preview?clear&redirect=${page}`} icon={'close'} className="small float-right" />
           </Message>
