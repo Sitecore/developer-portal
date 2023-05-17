243763cd2edf3728bf49d137256f4d62bcb70647
Mark van Aalst <markvanaalst@gmail.com>
2023-05-17 16:40:05 +0200

    Use next link component instead of anchor

--- apps/devportal/src/pages/changelog/[product]/[entry].tsx
+++ apps/devportal/src/pages/changelog/[product]/[entry].tsx
@@ -80,18 +80,18 @@ const ChangelogProduct = ({ currentProduct, changelogEntry }: ChangelogProps) =>
               <nav className="mb-8 mt-4 flex" aria-label="Breadcrumb">
                 <ol className="inline-flex items-center space-x-1 md:space-x-3">
                   <li className="inline-flex items-center">
-                    <a href="/changelog" className="text-theme-text-alt inline-flex items-center text-sm font-medium">
+                    <Link href="/changelog" className="text-theme-text-alt inline-flex items-center text-sm font-medium">
                       Changelog
-                    </a>
+                    </Link>
                   </li>
                   <li>
                     <div className="flex items-center">
                       <svg aria-hidden="true" className="text-theme-text-alt h-6 w-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                         <path fillRule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd"></path>
                       </svg>
-                      <a href={`/changelog/${getSlug(currentProduct.name)}`} className="text-theme-text-alt ml-1 text-sm font-medium md:ml-2">
+                      <Link href={`/changelog/${getSlug(currentProduct.name)}`} className="text-theme-text-alt ml-1 text-sm font-medium md:ml-2">
                         {currentProduct.name}
-                      </a>
+                      </Link>
                     </div>
                   </li>
                 </ol>
