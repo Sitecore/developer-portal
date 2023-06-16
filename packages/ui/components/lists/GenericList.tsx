30a7d0b58fbf90445f8669be34104e387c168adb
Mark van Aalst <markvanaalst@gmail.com>
2023-06-16 16:45:47 +0200

    add generic list for updates

---
+++ packages/ui/components/lists/GenericList.tsx
@@ -0,0 +1,38 @@
+// Global
+import Image from 'next/legacy/image';
+// Components
+import { GenericListData } from 'ui/common/types/genericList';
+import TextLink from 'ui/components/common/TextLink';
+
+type GenericListProps = {
+  className?: string;
+  data: GenericListData;
+};
+
+const GenericList = ({ className, data }: GenericListProps): JSX.Element => (
+  <div className={className}>
+    <h2 className="mb-4 heading-md">{data.title}</h2>
+    <p className="mb-6 text-lg text-theme-text-alt">{data.subtitle}</p>
+    <ul className="grid gap-6 md:grid-cols-3">
+      {data.content.map((link) => (
+        <li key={link.title} className="flex flex-col justify-between">
+          <div>
+            <div className="mb-4 border border-theme-border-alt bg-theme-bg-alt">
+              <Image src={link.img.src} alt={link.img.alt || ''} width="368" height="207" layout="responsive" />
+            </div>
+            <h3 className="mb-2 heading-sm">{link.title}</h3>
+            <p className="mb-6 text-sm text-theme-text-alt">{link.description}</p>
+          </div>
+          <div>
+            <TextLink href={link.href} text={link.linkText} />
+          </div>
+        </li>
+      ))}
+    </ul>
+  </div>
+);
+GenericList.defaultProps = {
+  className: '',
+};
+
+export default GenericList;
