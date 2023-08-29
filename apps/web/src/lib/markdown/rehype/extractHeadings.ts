96619ef10e82a56dc61f6d16c2506609e3d0c3ff
Mark van Aalst <mva@sitecore.net>
2023-08-29 13:53:42 +0200

    fix build errors

--- apps/web/src/lib/markdown/rehype/extractHeadings.ts
+++ apps/web/src/lib/markdown/rehype/extractHeadings.ts
@@ -1,18 +1,15 @@
-import { ExtractHeadingsConfig } from '@/src/interfaces/contentheading';
 import { headingRank } from 'hast-util-heading-rank';
 import { toString } from 'hast-util-to-string';
 import { Root } from 'hastscript/lib/core';
 import { visit } from 'unist-util-visit';
+import { ExtractHeadingsConfig } from '../../interfaces/contentheading';
 
 /**
  * Extracts headings from a unified tree.
  * To be used *AFTER* the `rehype-slug` plugin.
  */
 
-export default function rehypeExtractHeadings({
-  rank,
-  headings,
-}: ExtractHeadingsConfig): (tree: Root) => void {
+export default function rehypeExtractHeadings({ rank, headings }: ExtractHeadingsConfig): (tree: Root) => void {
   return (tree: Root) => {
     visit(tree, 'element', (node) => {
       if (headingRank(node) === rank && node.properties?.id) {
