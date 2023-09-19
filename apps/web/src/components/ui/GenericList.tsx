1c0e3bfb36d4a6e708355385e046edcdc1067397
Mark van Aalst <mva@sitecore.net>
2023-09-19 16:02:56 +0200

    add link title text

--- apps/web/src/components/ui/GenericList.tsx
+++ apps/web/src/components/ui/GenericList.tsx
@@ -62,7 +62,7 @@ export const GenericList = (props: GenericListData) => {
               </Stack>
             </CardBody>
             <CardFooter>
-              <TextLink text={item.linkText} href={item.href} title={item.linkText} />
+              <TextLink text={item.linkText} href={item.href} title={`${item.title}`} />
             </CardFooter>
           </Card>
         ))}
