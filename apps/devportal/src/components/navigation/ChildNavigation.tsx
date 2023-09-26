6a4f11ecb99630994bf8fd860bdc273c0bfddb26
Mark van Aalst <mva@sitecore.net>
2023-09-26 13:57:31 +0200

    add child navigation collapse

--- apps/devportal/src/components/navigation/ChildNavigation.tsx
+++ apps/devportal/src/components/navigation/ChildNavigation.tsx
@@ -1,10 +1,10 @@
-import { Box, Button, ButtonGroup, Heading, Hide, Menu, MenuButton, MenuItem, MenuList, Show, Stack, Text, Tooltip } from '@chakra-ui/react';
+import { Box, Button, ButtonGroup, Collapse, Heading, Hide, Icon, Menu, MenuButton, MenuItem, MenuList, Show, Stack, Text, Tooltip, useDisclosure } from '@chakra-ui/react';
 import React from 'react';
 //import { useGlobalState } from '../../lib/globalState';
 import { ChevronDownIcon } from '@chakra-ui/icons';
-import { mdiMenuDown } from '@mdi/js';
 import { default as Link, default as NextLink } from 'next/link';
-import { SubPageNavigation } from '../../lib/interfaces/page-info';
+import { useRouter } from 'next/router';
+import { SubPageNavigation, SubPageNavigationItem } from '../../lib/interfaces/page-info';
 
 interface ChildNavigationProps {
   title?: string;
@@ -12,10 +12,10 @@ interface ChildNavigationProps {
 }
 
 const ChildNavigation = ({ subPageNavigation }: ChildNavigationProps) => {
-  //const [navScrolled] = useGlobalState('navScrolled');
+  const { isOpen, onToggle } = useDisclosure();
+  const router = useRouter();
 
   return (
-    // <nav className={`mb-8 transform-gpu self-start transition-all md:sticky md:mr-16 ${positionalClasses} `}>
     <Box as="nav" position={'sticky'} top={'9rem'}>
       {/* Mobile */}
       <Hide above="md">
@@ -45,31 +45,32 @@ const ChildNavigation = ({ subPageNavigation }: ChildNavigationProps) => {
             {subPageNavigation.title}
           </Heading>
         )}
-        <ButtonGroup variant="navigation" orientation="vertical" spacing="1" m="-2.5" position={'sticky'}>
+        <ButtonGroup variant="navigation" orientation="vertical" spacing="1" m="-2.5" position={'sticky'} width={'full'}>
           {subPageNavigation?.routes.map((link, i) => {
             const urlSegment = `${subPageNavigation.path}/${link.path}`;
 
             return (
               <React.Fragment key={i}>
-                <Tooltip label={link.title} aria-label="A tooltip">
-                  <Button as={NextLink} href={urlSegment} key={i} rightIcon={<path d={mdiMenuDown} />}>
-                    <Text maxW={190} isTruncated>
-                      {link.title}
-                    </Text>
-                  </Button>
-                </Tooltip>
-                {link.children?.length > 0 && (
-                  <Stack paddingLeft={6}>
-                    {link.children?.map((child, i) => {
-                      return (
-                        <Tooltip label={child.title} aria-label="A tooltip" key={i}>
-                          <Button as={NextLink} href={`${urlSegment}/${child.path}`}>
-                            {child.title}
-                          </Button>
-                        </Tooltip>
-                      );
-                    })}
-                  </Stack>
+                {!link.children ? (
+                  <Tooltip label={link.title} aria-label="A tooltip">
+                    <Button as={NextLink} href={urlSegment} key={i}>
+                      <Text maxW={190} isTruncated>
+                        {link.title}
+                      </Text>
+                    </Button>
+                  </Tooltip>
+                ) : (
+                  <>
+                    <Tooltip label={link.title} aria-label="A tooltip">
+                      <Button rightIcon={<Icon onClick={onToggle} as={ChevronDownIcon} w={6} h={6} />} justifyContent={'space-between'} width={'full'}>
+                        <Link href={urlSegment}>{link.title}</Link>
+                      </Button>
+                    </Tooltip>
+                    {/* Child items */}
+                    <Collapse in={router.asPath == urlSegment ? !isOpen : isOpen} animateOpacity>
+                      {link.children?.length > 0 && renderChildren(link, urlSegment)}
+                    </Collapse>
+                  </>
                 )}
               </React.Fragment>
             );
@@ -79,5 +80,40 @@ const ChildNavigation = ({ subPageNavigation }: ChildNavigationProps) => {
     </Box>
   );
 };
+
+function renderChildren(link: SubPageNavigationItem, urlSegment: string): React.ReactNode {
+  const { isOpen, onToggle } = useDisclosure();
+
+  return (
+    <Stack paddingLeft={4}>
+      {link.children?.map((child, i) => {
+        return (
+          <React.Fragment key={i}>
+            {!child.children ? (
+              <Tooltip label={link.title} aria-label="A tooltip">
+                <Button as={NextLink} href={`${urlSegment}/${child.path}`} key={i}>
+                  <Text maxW={190} isTruncated>
+                    {child.title}
+                  </Text>
+                </Button>
+              </Tooltip>
+            ) : (
+              <React.Fragment key={i}>
+                <Tooltip label={child.title} aria-label="A tooltip" key={i}>
+                  <Button rightIcon={<Icon onClick={onToggle} as={ChevronDownIcon} w={6} h={6} />} justifyContent={'space-between'} width={'full'}>
+                    {child.path ? <Link href={`${urlSegment}/${child.path}`}>{child.title}</Link> : <Text>{child.title}</Text>}
+                  </Button>
+                </Tooltip>
+                <Collapse in={isOpen} animateOpacity>
+                  {child.children?.length > 0 && renderChildren(child, urlSegment)}
+                </Collapse>
+              </React.Fragment>
+            )}
+          </React.Fragment>
+        );
+      })}
+    </Stack>
+  );
+}
 
 export default ChildNavigation;
