3742482ac5f6354802a6c5c3bd4d793ef4f11380
Mark van Aalst <mva@sitecore.net>
2023-09-27 16:00:26 +0200

    remove twitterembed declaration

--- packages/ui/globals.d.ts
+++ packages/ui/globals.d.ts
@@ -1,8 +1,3 @@
 // Allow CSS modules for third-party libraries
 declare module '*.module.css';
 declare module '*.module.scss';
-
-// declare module 'react-twitter-embed' {
-//   const TwitterTweetEmbed: any;
-//   export default TwitterTweetEmbed;
-// }
