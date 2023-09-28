8722b927674a5fb6e33aaf97d2a562566c2eb0fd
Mark van Aalst <mva@sitecore.net>
2023-09-28 11:42:52 +0200

    update link texts

--- apps/devportal/data/data-platform.ts
+++ apps/devportal/data/data-platform.ts
@@ -5,7 +5,7 @@ const content: GenericListItem[] = [
     title: 'Experience Platform',
     description: 'Create connections, drive conversions, and foster loyalty. With customer data, analytics, marketing automation, and more, with the stand alone Sitecore Experience Platform™.',
     href: '/marketing-automation/experience-platform',
-    linkText: 'Learn more!',
+    linkText: 'Experience Platform',
     img: {
       src: '/images/platform/experience-platform.svg',
       alt: 'Experience Platform',
@@ -17,7 +17,7 @@ const content: GenericListItem[] = [
     title: 'Experience Manager',
     description: 'There’s no experience without content. Sitecore Experience Manager® is a powerful and intuitive CMS for the simplified creation and management of experiences across channels and devices.',
     href: '/content-management/experience-manager',
-    linkText: 'Learn more!',
+    linkText: 'Experience Manager',
     img: {
       src: '/images/platform/experience-manager.svg',
       alt: 'Experience Manager',
@@ -29,7 +29,7 @@ const content: GenericListItem[] = [
     title: 'Managed Cloud',
     description: 'Sitecore Managed Cloud service actively hosts, monitors, manages, and maintains the installation of Sitecore Experience Platform™ and Sitecore Experience Manager',
     href: '/devops/managed-cloud',
-    linkText: 'Learn more',
+    linkText: 'Managed Cloud',
     img: {
       src: '/images/platform/managed-cloud.svg',
       alt: 'Managed Cloud',
