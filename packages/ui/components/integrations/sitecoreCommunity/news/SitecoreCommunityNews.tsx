0733205426c573687ac1c4da2fe7390b874254ba
Mark van Aalst <mva@sitecore.net>
2023-10-02 09:53:26 +0200

    add LinkBox, fix padding

--- packages/ui/components/integrations/sitecoreCommunity/news/SitecoreCommunityNews.tsx
+++ packages/ui/components/integrations/sitecoreCommunity/news/SitecoreCommunityNews.tsx
@@ -19,15 +19,15 @@ export const SitecoreCommunityNews = ({ title, data, hideCategory, listItem, ...
   }
 
   return (
-    <Card shadow={'none'} {...rest} background={'transparent'}>
-      <CardHeader justifyContent={'space-between'} display={'flex'}>
+    <Card variant={'flat'} shadow={'none'}>
+      <CardHeader justifyContent={'space-between'} display={'flex'} px="0">
         <Heading as={'h3'} size={'xl'}>
           {title ? title : 'News and Announcements'}
         </Heading>
         <TextLink href={'https://community.sitecore.com/community?id=community_forum&sys_id=af85dddf1bf17810486a4083b24bcb00'} text={'See all'} />
       </CardHeader>
-      <CardBody>
-        <Stack spacing={8} direction={{ base: 'column', md: 'row' }}>
+      <CardBody p="0">
+        <Stack spacing={8} direction={{ base: 'column', md: 'row' }} py={5}>
           {data.map((item, i) =>
             listItem ? (
               <SitecoreCommunityNewsOrEventItemSidebar {...item} startDate={item.publishDate} key={i} />
