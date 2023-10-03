edf5aa3e83aafdbdf0db5ece00413b0dc2744f4e
Mark van Aalst <mva@sitecore.net>
2023-10-03 14:34:30 +0200

    fix icon reference

--- apps/devportal/src/layouts/Layout.tsx
+++ apps/devportal/src/layouts/Layout.tsx
@@ -22,7 +22,7 @@ const Layout = ({ title, description = '', openGraphImage, children, ...rest }: 
     <>
       <Head>
         <title>{title}</title>
-        <link rel="icon" href={`/favicon.png`} />
+        <link rel="icon" href={`/favicon.ico`} />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
