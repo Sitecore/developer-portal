bc9237445068fc6f4ad5bf5e01ac010a95957023
Dylan Young <5227854+dylanyoung-dev@users.noreply.github.com>
2023-10-18 11:40:15 -0400

    Remove Hardcoded values

--- packages/ui/components/integrations/engage/EngageKeys.ts
+++ packages/ui/components/integrations/engage/EngageKeys.ts
@@ -5,10 +5,9 @@ export interface IEngageConfigKeys {
   SitecoreCdpTargetUrl: string;
 }
 
-//TODO: Hardcoded values since the build is broken and I can't get these to load from the .env file
 export const EngageKeys: IEngageConfigKeys = {
-  SitecoreCdpClientKey: process.env.NEXT_PUBLIC_SITECORE_CDP_CLIENT_KEY ?? 'demus01flt27azgg4uvrms0rb5mkautx',
-  SitecoreCdpPointOfSale: process.env.NEXT_PUBLIC_SITECORE_CDP_POS ?? 'devPortalTemp',
-  SitecoreCdpCookieDomain: process.env.NEXT_PUBLIC_SITECORE_CDP_COOKIE_DOMAIN ?? 'localhost',
-  SitecoreCdpTargetUrl: process.env.NEXT_PUBLIC_SITECORE_CDP_TARGETURL ?? 'https://api-engage-us.sitecorecloud.io',
+  SitecoreCdpClientKey: process.env.NEXT_PUBLIC_SITECORE_CDP_CLIENT_KEY ?? '',
+  SitecoreCdpPointOfSale: process.env.NEXT_PUBLIC_SITECORE_CDP_POS ?? '',
+  SitecoreCdpCookieDomain: process.env.NEXT_PUBLIC_SITECORE_CDP_COOKIE_DOMAIN ?? '',
+  SitecoreCdpTargetUrl: process.env.NEXT_PUBLIC_SITECORE_CDP_TARGETURL ?? '',
 };
