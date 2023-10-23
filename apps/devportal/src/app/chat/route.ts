b8847576edd43e723e4fe725816da9edaf54798d
Dylan Young <5227854+dylanyoung-dev@users.noreply.github.com>
2023-10-23 12:43:23 -0400

    Using new model type in routes

--- apps/devportal/src/app/chat/route.ts
+++ apps/devportal/src/app/chat/route.ts
@@ -1,18 +1,17 @@
-import { IExperienceResult } from '@/src/components/chatbot/IExperienceResult';
+import { IPersonalizedExperience } from '@/src/components/chatbot/IExperienceResult';
 import { NextResponse } from 'next/server';
 import { Message, MessageType } from '../../types/Message';
 import { AzureOpenAI } from './azure-openai';
 
 export interface IChatGPTPayload {
   query: string;
-  context: IExperienceResult | undefined;
+  context: IPersonalizedExperience | undefined;
   history: Message[];
 }
 
 export async function POST(request: Request) {
   const data = (await request.json()) as IChatGPTPayload;
   const formatMessage = ' Format this answer as MarkDown, but dont mention it in the reponse';
-  const { query, context, history }: { query: string; context: IExperienceResult | undefined; history: Message[] } = data;
 
   if (!data.query) {
     return new NextResponse('Please provide a query parameter', { status: 400 });
@@ -21,21 +20,20 @@ export async function POST(request: Request) {
   // Use Context to fill in the LLM with more context data
   const messages = [{ role: 'system', content: 'You are a helpful assistant, designed to help Developers building with Sitecore.' }];
 
-  if (context?.persona) {
-    //messages.push({ role: 'user', content: `I'm a ${context.persona.Name} and I value these responses: ${//context.persona.CommonAttributes.join(', ')}` });
-    messages.push({ role: 'user', content: `I'm a ${context.persona} and I value these responses: code samples vs marketing fluff` });
+  if (data.context?.persona) {
+    messages.push({ role: 'user', content: `I'm a ${data.context.persona} and I value these responses: ${data.context.persona.CommonAttributes.join(',')}` });
   }
 
   if (data.context?.recent_search_summary) {
     messages.push({ role: 'user', content: `I've recently searched for ${data.context.recent_search_summary}` });
   }
 
-  if (context?.relevant_tags) {
-    messages.push({ role: 'user', content: `I've recently been interested in the following topics: ${context.relevant_tags}` });
+  if (data.context?.relevant_tags) {
+    messages.push({ role: 'user', content: `I've recently been interested in the following topics: ${data.context.relevant_tags}` });
   }
 
-  if (context?.product_details) {
-    messages.push({ role: 'user', content: `I've recently viewed the following products: ${context.product_details}` });
+  if (data.context?.product) {
+    messages.push({ role: 'user', content: `I've recently viewed the following products: ${data.context.product.name} in the ${data.context.product.cloud}` });
   }
 
   if (data.history?.length > 0) {
