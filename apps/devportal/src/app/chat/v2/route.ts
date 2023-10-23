775efbba439bd619f16f939a6820252156de7e64
Mark van Aalst <mva@sitecore.net>
2023-10-23 15:22:01 +0200

    streaming experiment

---
+++ apps/devportal/src/app/chat/v2/route.ts
@@ -0,0 +1,55 @@
+import { IExperienceResult } from '@/src/components/chatbot/IExperienceResult';
+import { Message, MessageType } from '../../../types/Message';
+import { AzureOpenAI } from './azure-openai';
+
+export interface IChatGPTPayload {
+  query: string;
+  context: IExperienceResult | undefined;
+  history: Message[];
+}
+
+export async function POST(request: Request) {
+  const data = (await request.json()) as IChatGPTPayload;
+  const formatMessage = ' and output the answer in markdown text';
+
+  // Use Context to fill in the LLM with more context data
+  const messages = [{ role: 'system', content: 'You are a helpful assistant, designed to help Developers building with Sitecore.' }];
+
+  if (data.context?.persona) {
+    //messages.push({ role: 'user', content: `I'm a ${context.persona.Name} and I value these responses: ${//context.persona.CommonAttributes.join(', ')}` });
+    messages.push({ role: 'user', content: `I'm a ${data.context.persona.Name} and I value these responses: code samples vs marketing fluff` });
+  }
+
+  //   if (context?.recent_search_context) {
+  //     messages.push({ role: 'user', content: `I've recently searched for ${context.recent_search_context}` });
+  //   }
+
+  if (data.context?.relevant_tags) {
+    messages.push({ role: 'user', content: `I've recently been interested in the following topics: ${data.context.relevant_tags}` });
+  }
+
+  //   if (context?.recent_products) {
+  //     messages.push({ role: 'user', content: `I've recently viewed the following products: ${context.recent_products.product}` });
+  //   }
+
+  if (data.history?.length > 0) {
+    for (const item of data.history) {
+      if (item.type == MessageType.Assistant) {
+        messages.push({ role: 'assistant', content: item.text });
+      } else {
+        messages.push({ role: 'user', content: item.text });
+      }
+    }
+  }
+  messages.push({ role: 'user', content: data.query });
+  //messages.push({ role: 'system', content: 'you output the message in html' });
+
+  const azure = new AzureOpenAI();
+
+  const stream = await azure.createChatCompletion({
+    messages: messages,
+    stream: true, // stream the response
+  });
+
+  return new Response(stream);
+}
---
+++ apps/devportal/src/components/chatbot/ChatBot2.tsx
@@ -0,0 +1,191 @@
+/* eslint-disable no-constant-condition */
+import { Message, MessageType } from '@/src/types/Message';
+import { Box, Button, Card, CardBody, CardFooter, CardHeader, CloseButton, FormControl, Heading, IconButton, Input, Progress, Stack, Text, Tooltip, Wrap, useBoolean } from '@chakra-ui/react';
+import { mdiCreation, mdiDeleteSweep } from '@mdi/js';
+import Icon from '@mdi/react';
+import { useCallback, useEffect, useRef, useState } from 'react';
+import { useEngageTracker } from 'ui/components/integrations';
+import { IExperienceResult } from './IExperienceResult';
+import { Messages } from './Messages';
+
+type ChatBotProps = {
+  onClose?: () => void;
+  isOpen?: boolean;
+};
+
+const intialMessage = [
+  {
+    type: MessageType.Assistant,
+    text: "Hello! I'm Clippy, your friendly Sitecore helper. How can I help you today?",
+  },
+];
+
+export const ChatBot2 = ({ onClose, isOpen }: ChatBotProps) => {
+  const tracker = useEngageTracker();
+  const [isLoading, setIsLoading] = useBoolean(false);
+  const [question, setQuestion] = useState('');
+  const [personaContext, setPersonaContext] = useState<IExperienceResult | undefined>();
+  const [messages, setMessage] = useState<Message[]>(intialMessage);
+  const isBlank = question === '';
+
+  const requestBody = {
+    query: question,
+    history: messages,
+    context: personaContext,
+  };
+
+  const messagesEndRef = useRef<null | HTMLDivElement>(null);
+
+  const scrollToBottom = () => {
+    messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' });
+  };
+
+  const handlePersonalizeContext = useCallback(async () => {
+    if (tracker.context.isTrackerEnabled) {
+      const result = await tracker.RunPersonalizationFlow<IExperienceResult>('developer_portal_scai_experience*');
+
+      if (result) {
+        setPersonaContext(result);
+        console.log(result);
+      }
+    }
+  }, []);
+
+  useEffect(() => {
+    handlePersonalizeContext();
+  }, [handlePersonalizeContext]);
+
+  useEffect(() => {
+    if (isOpen) {
+      scrollToBottom();
+    }
+  }, [messages]);
+
+  const storeQuestionPersonalize = async (question: string) => {
+    if (tracker.context.isTrackerEnabled) {
+      await tracker.TrackEvent('ClippySearch', {
+        question: question,
+      });
+    }
+  };
+
+  const askQuestion = async () => {
+    if (isBlank) return;
+    setMessage((old) => [...old, { type: MessageType.User, text: question }]);
+
+    setIsLoading.on();
+
+    await storeQuestionPersonalize(question);
+
+    // TODO: Move this to a lib/service
+    const result: Response = await fetch('/chat/v2', {
+      method: 'POST',
+      body: JSON.stringify(requestBody),
+    });
+
+    if (!result.ok) throw new Error(result.statusText);
+    if (result.body == null) throw new Error('No body in response');
+
+    const reader = result.body.getReader();
+    const decoder = new TextDecoder();
+
+    let completeResponse = '';
+    //const message
+
+    while (true) {
+      const { value, done: doneReading } = await reader.read();
+      if (doneReading) {
+        break;
+      }
+
+      const chunkValue = decoder.decode(value);
+      completeResponse += chunkValue;
+
+      const lastMessage = messages[messages.length - 1];
+      const updatedMessage = { ...lastMessage, text: completeResponse };
+
+      // if the last message was from the assistant, it's the same message, so update it
+
+      setMessage((old) => {
+        if (lastMessage.type === MessageType.Assistant) {
+          const lastMessage = old[old.length - 1];
+          const updatedMessage = { ...lastMessage, text: completeResponse };
+          return [...old.slice(0, -1), updatedMessage];
+        } else {
+          return [...old, { type: MessageType.Assistant, text: completeResponse }];
+        }
+      });
+    }
+    setIsLoading.off();
+    scrollToBottom();
+    //setQuestion('');
+  };
+
+  const resetSession = function () {
+    setMessage(() => [
+      {
+        type: MessageType.Assistant,
+        text: "Hello! I'm Clippy, your friendly Sitecore helper. How can I help you today?",
+      },
+    ]);
+    setQuestion('');
+  };
+
+  return (
+    <Card variant={'elevated'} size={['sm', 'md', 'lg']} maxW={'lg'} background={'transparent'}>
+      {/* <CardHeader background="primary-fg" color="chakra-inverse-text" borderTopRadius="2xl"> */}
+      <CardHeader bgGradient="linear(to-tr, primary.500, teal.500)" color="chakra-inverse-text" borderTopRadius="2xl">
+        <Stack direction="row" spacing={4} align="top" justify="space-between">
+          <Stack>
+            <Heading as="h2">Sitecore Clippy</Heading>
+            <Text variant="subtle" color="chakra-inverse-text">
+              your friendly Sitecore helper!
+            </Text>
+          </Stack>
+          <CloseButton onClick={onClose} />
+        </Stack>
+      </CardHeader>
+      <CardBody height={'200px'} background={'chakra-body-bg'}>
+        <Box overflowY="auto" maxHeight={['200px', '400px', '600px']}>
+          <Messages messages={messages} />
+          {isLoading && <Progress mx={5} mt={4} variant="ai" isIndeterminate />}
+          <div ref={messagesEndRef} />
+        </Box>
+      </CardBody>
+
+      <CardFooter gap={4} background={'chakra-body-bg'} borderBottomRadius={'xl'}>
+        <Tooltip label="Reset">
+          <IconButton onClick={resetSession} variant="outline" icon={<Icon path={mdiDeleteSweep} size={1} />} marginTop={'auto'} aria-label="Reset">
+            Reset
+          </IconButton>
+        </Tooltip>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            askQuestion();
+          }}
+          style={{ width: '100%' }}
+        >
+          <FormControl>
+            <Input variant="outline" type="text" placeholder="What would you like to know?" value={question} id="question" onChange={(e) => setQuestion(e.target.value)} focusBorderColor="primary-subtle-bg" />
+          </FormControl>
+        </form>
+        <Wrap align="center">
+          <Button
+            isDisabled={isLoading}
+            _hover={{}}
+            onClick={(e) => {
+              e.preventDefault();
+              askQuestion();
+            }}
+            variant="ai"
+            leftIcon={<Icon path={mdiCreation} size={1} />}
+            marginTop={'auto'}
+          >
+            Ask
+          </Button>
+        </Wrap>
+      </CardFooter>
+    </Card>
+  );
+};
