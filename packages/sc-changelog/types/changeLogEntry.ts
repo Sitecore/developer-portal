52b169139a2390a80e04fee6f1209604fdf2f062
Rob Earlam <robert.earlam@gmail.com>
2023-12-01 11:20:29 +1100

    Removed temporary model fields

--- packages/sc-changelog/types/changeLogEntry.ts
+++ packages/sc-changelog/types/changeLogEntry.ts
@@ -16,13 +16,11 @@ export type ChangelogEntrySummary = {
   id: string;
   title: string;
   releaseDate: string;
-  releasedate: string;
   lightIcon: string;
   darkIcon: string;
   productName: string | null;
   products: SitecoreProduct[] | null;
   changeTypeName: string | null;
-  changetypename: string | null;
 };
 
 export type ChangelogEntry = ChangelogEntrySummary & {
@@ -72,13 +70,11 @@ function parseChangeLogSummaryItem(changelog: ChangelogBase): ChangelogEntrySumm
     id: changelog.id,
     title: changelog.title,
     releaseDate: new Date(changelog.releaseDate).toLocaleDateString(['en-US'], { year: 'numeric', month: 'short', day: 'numeric' }),
-    releasedate: new Date(changelog.releaseDate).toLocaleDateString(['en-US'], { year: 'numeric', month: 'short', day: 'numeric' }),
     lightIcon: changelog.sitecoreProduct.results[0]?.lightIcon,
     darkIcon: changelog.sitecoreProduct.results[0]?.darkIcon,
     productName: changelog.sitecoreProduct.results[0]?.productName ?? null,
     products: changelog.sitecoreProduct.results ?? null,
     changeTypeName: changelog.changeType.results[0]?.changeType ?? null,
-    changetypename: changelog.changeType.results[0]?.changeType ?? null,
   };
 }
 
@@ -95,13 +91,11 @@ export function parseChangeLogItem(changelog: Changelog): ChangelogEntry {
     changeType: changelog.changeType.results,
     version: changelog.version,
     releaseDate: new Date(changelog.releaseDate).toLocaleDateString(['en-US'], { year: 'numeric', month: 'short', day: 'numeric' }),
-    releasedate: new Date(changelog.releaseDate).toLocaleDateString(['en-US'], { year: 'numeric', month: 'short', day: 'numeric' }),
     image: changelog.image.results,
     lightIcon: changelog.sitecoreProduct.results[0]?.lightIcon,
     darkIcon: changelog.sitecoreProduct.results[0]?.darkIcon,
     productName: changelog.sitecoreProduct.results[0]?.productName ?? null,
     products: changelog.sitecoreProduct.results ?? null,
     changeTypeName: changelog.changeType.results[0]?.changeType ?? null,
-    changetypename: changelog.changeType.results[0]?.changeType ?? null,
   };
 }
