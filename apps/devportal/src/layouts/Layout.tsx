18714b88223948460b3d63fd8c382e9838750a41
Mark van Aalst <mva@sitecore.net>
2023-12-05 09:44:58 +0100

    add downloads page make footer stick to bottom

--- apps/devportal/src/layouts/Layout.tsx
+++ apps/devportal/src/layouts/Layout.tsx
@@ -43,7 +43,7 @@ const Layout = ({ title, description = '', openGraphImage, children, ...rest }: 
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
 
-      <Box as="main" style={{ minHeight: 'calc(100vh - 50px)' }} {...rest}>
+      <Box as="main" {...rest}>
         <VisuallyHidden>
           <a href="#main-content">Skip to main content</a>
         </VisuallyHidden>
