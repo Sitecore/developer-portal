4dc3af0d2cd3017951eebe0ea0ba677d49faa2db
Mark van Aalst <mva@sitecore.net>
2023-12-06 15:47:25 +0100

    add custom 404 page

--- apps/devportal/src/components/sitecore-search/SearchInput.tsx
+++ apps/devportal/src/components/sitecore-search/SearchInput.tsx
@@ -1,12 +1,16 @@
 // Global
-import { FormControl, Input, InputGroup, InputLeftElement, InputRightElement, Text } from '@chakra-ui/react';
+import { Button, FormControl, HStack, Input, InputGroup, InputLeftElement, InputRightElement, Text } from '@chakra-ui/react';
 import { useRouter } from 'next/router';
 import { useState } from 'react';
 import { FaSearch } from 'react-icons/fa';
 import ProductLogo from 'ui/components/common/ProductLogo';
 import { Product } from 'ui/lib/assets';
 
-const SearchInput = () => {
+export interface SearchInputProps {
+  showButton?: boolean;
+}
+
+const SearchInput = ({ showButton }: SearchInputProps) => {
   const router = useRouter();
   const [keywords, setKeywords] = useState(router.query['q'] ?? '');
 
@@ -15,33 +19,48 @@ const SearchInput = () => {
     router.push('/search?q=' + keywords).then(() => router.reload());
   };
 
+  const inputBox = (
+    <InputGroup width={'full'} rounded={'none'}>
+      <InputLeftElement>
+        <FaSearch />
+      </InputLeftElement>
+      <Input
+        name="scdp-search"
+        autoComplete="off"
+        value={keywords}
+        onChange={(event) => {
+          setKeywords(event.target.value);
+        }}
+      />
+      <InputRightElement width={{ base: '100px', md: '150px' }} opacity={'0.5'}>
+        <Text as={'span'} display={{ base: 'none', md: 'flex ' }} variant="tiny">
+          Powered by
+        </Text>
+
+        <ProductLogo product={Product.Search} width={67} height={18} />
+      </InputRightElement>
+    </InputGroup>
+  );
+
+  if (showButton)
+    return (
+      <FormControl onSubmit={submit} as="form">
+        <HStack>
+          {inputBox}
+          <Button onClick={submit}>Search</Button>
+        </HStack>
+      </FormControl>
+    );
+
   return (
     <FormControl onSubmit={submit} as="form">
-      <InputGroup width={'full'} rounded={'none'}>
-        <InputLeftElement>
-          <FaSearch />
-        </InputLeftElement>
-        <Input
-          name="scdp-search"
-          autoComplete="off"
-          value={keywords}
-          onChange={(event) => {
-            setKeywords(event.target.value);
-          }}
-        />
-        <InputRightElement width={{ base: '100px', md: '150px' }} opacity={'0.5'}>
-          <Text as={'span'} display={{ base: 'none', md: 'flex ' }} variant="tiny">
-            Powered by
-          </Text>
-
-          <ProductLogo product={Product.Search} width={67} height={18} />
-        </InputRightElement>
-      </InputGroup>
+      {inputBox}
     </FormControl>
   );
 };
 SearchInput.defaultProps = {
   className: '',
 };
+
 
 export default SearchInput;
