eed36bf7a6718ea1253122ec2ae789849ab23ee3
Mark van Aalst <mva@sitecore.net>
2023-12-17 22:53:41 +0100

    Use full version of syntax highlight

--- apps/devportal/src/components/markdown/MarkdownContent.tsx
+++ apps/devportal/src/components/markdown/MarkdownContent.tsx
@@ -5,13 +5,13 @@ import { mdiSquareEditOutline } from '@mdi/js';
 import { Icon } from '@mdi/react';
 import { Prose } from '@nikolovlazar/chakra-ui-prose';
 import { MDXRemote } from 'next-mdx-remote';
-import { Light as SyntaxHighlight } from 'react-syntax-highlighter';
 import { Article, LinkItem, Repository } from 'ui/components/cards';
 import { Row } from 'ui/components/helpers/Row';
 import { ButtonLink } from 'ui/components/links/ButtonLink';
 import { Promo, VideoPromo } from 'ui/components/promos';
 import YouTube from 'ui/components/video/YouTube';
 
+import SyntaxHighlighter from 'react-syntax-highlighter';
 import { Download } from 'ui/components/cards/Download';
 import { Group } from 'ui/components/cards/Group';
 import styles from './MarkdownContent.module.css'; /* eslint-disable react/no-unknown-property */
@@ -47,9 +47,9 @@ export const DecoratedMarkdown = ({ children }: DecoratedMarkdownProps): JSX.Ele
             const match = /language-(\w+)/.exec(className || '');
             const lang = match ? match[1] : '';
             return match ? (
-              <SyntaxHighlight style={a11yDark} language={lang} className="no-prose" PreTag={'div'} customStyle={{ background: 'inherit', display: 'inline-grid', width: '100%' }} wrapLongLines wrapLines>
+              <SyntaxHighlighter style={a11yDark} language={lang} className="no-prose" PreTag={'div'} customStyle={{ background: 'inherit', display: 'inline-grid', width: '100%' }} wrapLongLines wrapLines>
                 {String(children).replace(/\n$/, '')}
-              </SyntaxHighlight>
+              </SyntaxHighlighter>
             ) : (
               <code className={className}>{children}</code>
             );
