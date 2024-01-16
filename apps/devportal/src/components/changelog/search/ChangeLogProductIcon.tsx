3ac3179e1fd2eb5b36bb8abdaf22163a83cf5d24
Rob Earlam <robert.earlam@gmail.com>
2024-01-17 09:51:10 +1100

    Changelog Search product links now working

---
+++ apps/devportal/src/components/changelog/search/ChangeLogProductIcon.tsx
@@ -0,0 +1,21 @@
+import { HStack, Text, Tooltip, useColorModeValue } from '@chakra-ui/react';
+import Image from 'next/image';
+import Link from 'next/link';
+import SitecoreProduct from 'sc-changelog/types/sitecoreProduct';
+
+type ProductIconProps = {
+  product: SitecoreProduct;
+};
+
+export const ChangeLogProductIcon = ({ product }: ProductIconProps): JSX.Element => {
+  return (
+    <Tooltip label={`Go to the ${product.productName} changelog page`} aria-label="A tooltip">
+      <Link href={`/changelog-search/${product.productName}`}>
+        <HStack>
+          <Image src={useColorModeValue(product.lightIcon, product.darkIcon)} alt={product.productName} width={20} height={20} priority={true} />
+          <Text>{product.productName}</Text>
+        </HStack>
+      </Link>
+    </Tooltip>
+  );
+};
--- apps/devportal/src/components/changelog/search/ChangelogSearchItemMeta.tsx
+++ apps/devportal/src/components/changelog/search/ChangelogSearchItemMeta.tsx
@@ -3,7 +3,7 @@ import { usePreview } from '@/src/context/PreviewContext';
 import { Alert, BoxProps, Button, HStack, Icon, Link, Tag, TagLabel, Tooltip } from '@chakra-ui/react';
 import { mdiSquareEditOutline } from '@mdi/js';
 import { ChangelogEntry } from 'sc-changelog/types/changeLogEntry';
-import { ProductIcon } from '../ProductIcon';
+import { ChangeLogProductIcon } from './ChangeLogProductIcon';
 
 type ChangelogItemMetaProps = BoxProps & {
   loading?: boolean;
@@ -26,7 +26,7 @@ export const ChangelogSearchItemMeta = ({ item, ...rest }: ChangelogItemMetaProp
 
   const MetaInfo = (
     <HStack {...rest} gap={4}>
-      {item.products != null ? item.products.map((product, key) => <ProductIcon product={product} key={key} />) : <Alert status="error">No product defined</Alert>}
+      {item.products != null ? item.products.map((product, key) => <ChangeLogProductIcon product={product} key={key} />) : <Alert status="error">No product defined</Alert>}
 
       <time dateTime="2022-10-21T15:48:00.000Z">{new Date(item.releaseDate).toDateString()}</time>
 
