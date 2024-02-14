808bee80789a8bfcef4553acc3472b86d99f47de
Mark van Aalst <mva@sitecore.net>
2024-02-14 17:03:56 +0100

    refactored sidebar navigation for consistency

--- apps/devportal/src/layouts/ArticlePage.tsx
+++ apps/devportal/src/layouts/ArticlePage.tsx
@@ -1,15 +1,15 @@
 import { TrackPageView } from '@/src/components/engagetracker/TrackPageView';
 import { ContentHeading } from '@lib/interfaces/contentheading';
-import { ChildPageInfo, PageInfo, PagePartialGroup, PartialData, SubPageNavigation } from '@lib/interfaces/page-info';
+import { ChildPageInfo, PageInfo, PagePartialGroup, PartialData, SidebarNavigationConfig } from '@lib/interfaces/page-info';
 import SocialFeeds from '@src/components/common/SocialFeeds';
 import { MarkDownContent } from '@src/components/markdown/MarkdownContent';
-import ChildNavigation from '@src/components/navigation/ChildNavigation';
 import InPageNav from '@src/components/navigation/InPageNav';
 import Layout from '@src/layouts/Layout';
 import Hero from 'ui/components/common/Hero';
 import { PromoCardProps } from 'ui/components/promos';
 import PromoList from 'ui/components/promos/promoCard/PromoList';
 import GithubContributionNotice from '../components/common/contribute';
+import SidebarNavigation from '../components/navigation/SidebarNavigation';
 import { ThreeColumnLayout } from './ThreeColumnLayout';
 
 type ArticlePageProps = {
@@ -20,12 +20,12 @@ type ArticlePageProps = {
   promoAfter?: PromoCardProps[];
   promoBefore?: PromoCardProps[];
   childPageInfo?: ChildPageInfo[];
-  subPageNavigation: SubPageNavigation;
+  sidebarConfig: SidebarNavigationConfig;
   customNav?: React.ReactNode;
   customNavPager?: React.ReactNode;
 };
 
-const ArticlePage = ({ pageInfo, partials, partialGroups, promoAfter, promoBefore, customNav, customNavPager, subPageNavigation }: ArticlePageProps) => {
+const ArticlePage = ({ pageInfo, partials, partialGroups, promoAfter, promoBefore, customNav, customNavPager, sidebarConfig }: ArticlePageProps) => {
   if (!pageInfo) return <>No pageInfo found</>;
 
   // Check for headings in the content
@@ -40,7 +40,7 @@ const ArticlePage = ({ pageInfo, partials, partialGroups, promoAfter, promoBefor
       <Layout title={pageInfo.title} description={pageInfo.description} openGraphImage={pageInfo.openGraphImage}>
         <Hero title={pageInfo.title} description={pageInfo.description} image={pageInfo.heroImage} productLogo={pageInfo.productLogo} />
 
-        <ThreeColumnLayout sidebar={pageInfo.hasSubPageNav && <ChildNavigation subPageNavigation={subPageNavigation} />} inPageLinks={sectionTitles} inPageNav={sectionTitles.length > 0 && Nav}>
+        <ThreeColumnLayout sidebar={pageInfo.hasSubPageNav && <SidebarNavigation config={sidebarConfig} />} inPageLinks={sectionTitles} inPageNav={sectionTitles.length > 0 && Nav}>
           <PromoList data={promoBefore} />
 
           <MarkDownContent content={pageInfo.parsedContent} partialGroups={partialGroups} partials={partials} />
