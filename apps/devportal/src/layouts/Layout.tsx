cd854a207f7b419e8bac01e49c869a1fd5ba1055
Mark van Aalst <mva@sitecore.net>
2024-03-14 10:16:04 +0100

    wrap meta component

--- apps/devportal/src/layouts/Layout.tsx
+++ apps/devportal/src/layouts/Layout.tsx
@@ -1,6 +1,6 @@
 // Global
 import { Alert, AlertDescription, AlertIcon, Box, BoxProps, VisuallyHidden } from '@chakra-ui/react';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import Meta from '../components/common/Meta';
 import { usePreview } from '../context/PreviewContext';
 
@@ -14,35 +14,24 @@ type LayoutProps = BoxProps & {
 
 const Layout = ({ title, description = '', openGraphImage, children, ...rest }: LayoutProps) => {
   const { isPreview } = usePreview();
-  const [mounted, setMounted] = useState(false);
-
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
-  if (!mounted) {
-    return null;
-  }
 
   return (
-    <>
+    <Box as="main" {...rest}>
       <Meta title={title} description={description} openGraphImage={openGraphImage} />
 
-      <Box as="main" {...rest}>
-        <VisuallyHidden>
-          <a href="#main-content">Skip to main content</a>
-        </VisuallyHidden>
-        {/* a11y announcement for route changes. */}
-        <VisuallyHidden aria-live="polite" aria-atomic="true">{`The ${title} page has loaded.`}</VisuallyHidden>
-        {isPreview && (
-          <Alert status="warning">
-            <AlertIcon />
-            <AlertDescription>Preview mode enabled</AlertDescription>
-          </Alert>
-        )}
-        {children}
-      </Box>
-    </>
+      <VisuallyHidden>
+        <a href="#main-content">Skip to main content</a>
+      </VisuallyHidden>
+      {/* a11y announcement for route changes. */}
+      <VisuallyHidden aria-live="polite" aria-atomic="true">{`The ${title} page has loaded.`}</VisuallyHidden>
+      {isPreview && (
+        <Alert status="warning">
+          <AlertIcon />
+          <AlertDescription>Preview mode enabled</AlertDescription>
+        </Alert>
+      )}
+      {children}
+    </Box>
   );
 };
 
