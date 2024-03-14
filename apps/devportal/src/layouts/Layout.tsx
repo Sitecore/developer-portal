7136568bdbdb476843b9ff15b5026988400b8a6a
Mark van Aalst <mva@sitecore.net>
2024-03-14 10:00:16 +0100

    OG testing

--- apps/devportal/src/layouts/Layout.tsx
+++ apps/devportal/src/layouts/Layout.tsx
@@ -1,8 +1,7 @@
 // Global
 import { Alert, AlertDescription, AlertIcon, Box, BoxProps, VisuallyHidden } from '@chakra-ui/react';
-import Head from 'next/head';
-import { useRouter } from 'next/router';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Meta from '../components/common/Meta';
 import { usePreview } from '../context/PreviewContext';
 
 type LayoutProps = BoxProps & {
@@ -13,42 +12,21 @@ type LayoutProps = BoxProps & {
   children: React.ReactNode | React.ReactNode[];
 };
 
-const Layout = ({ title, description = '', openGraphImage, children, ...rest }: LayoutProps): JSX.Element => {
+const Layout = ({ title, description = '', openGraphImage, children, ...rest }: LayoutProps) => {
   const { isPreview } = usePreview();
-  const publicUrl = process.env.NEXT_PUBLIC_PUBLIC_URL ? process.env.NEXT_PUBLIC_PUBLIC_URL : '';
-  const router = useRouter();
-  const { asPath } = router;
-  const path = asPath.split(/[?#]/)[0];
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
 
   return (
     <>
-      <Head>
-        <title>{title}</title>
-        <link rel="icon" href={`/favicon.ico`} />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="manifest" href="/site.webmanifest" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5548d9" />
-        <meta name="msapplication-TileColor" content="#5548d9"></meta>
-        <meta name="theme-color" content="#5548d9"></meta>
-
-        <meta name="description" content={description} />
-        {/*  Facebook Meta Tags */}
-        <meta property="og:site_name" content="Sitecore Developer Portal" />
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content={title} />
-        <meta property="og:description" content={description} />
-        <meta property="og:url" content={`${publicUrl}${path}`} />
-        <meta property="og:image" content={openGraphImage ? `${publicUrl}${openGraphImage}` : `${publicUrl}/api/og?title=${title}&subtitle=${description}`} />
-        {/* Twitter Meta Tags */}
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta property="twitter:domain" content="developers.sitecore.com" />
-        <meta property="twitter:url" content={`${publicUrl}${path}`} />
-        <meta name="twitter:title" content={title} />
-        <meta name="twitter:description" content={description} />
-        <meta name="twitter:image" content={openGraphImage ? `${publicUrl}${openGraphImage}` : `${publicUrl}/api/og?title=${title}&subtitle=${description}`} />
-      </Head>
+      <Meta title={title} description={description} openGraphImage={openGraphImage} />
 
       <Box as="main" {...rest}>
         <VisuallyHidden>
