ddf71eed643e51eabf42be3b04fc951c6081a75f
Mark van Aalst <mva@sitecore.net>
2024-03-18 16:58:05 +0100

    fix forward slash in path

--- apps/devportal/src/components/newsletter/NewsletterNav.tsx
+++ apps/devportal/src/components/newsletter/NewsletterNav.tsx
@@ -18,7 +18,7 @@ const NewsletterNav = ({ paths, currentYear, currentMonth }: NewsletterNavProps)
       active: parseInt(currentMonth, 10) == parseInt(item.params.month, 10) && parseInt(currentYear, 10) == parseInt(item.params.year, 10),
       // Set the dates as the 3rd of each month to avoid having to deal with timezones rolling it backwards
       text: translateDateAsYearMonth(`${item.params.year}-${item.params.month}-03`),
-      href: `/newsletter/${item.params.year}/${item.params.month}`,
+      href: `newsletter/${item.params.year}/${item.params.month}`,
     }))
     .sort((a, b) => {
       const yearDiff = b.year - a.year;
