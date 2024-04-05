62ac7d40856c87b8b0b83fdf1d7b6670537cd14d
Mark van Aalst <mva@sitecore.net>
2024-04-05 16:48:58 +0200

    switch to fs

--- apps/devportal/src/lib/newsletter.ts
+++ apps/devportal/src/lib/newsletter.ts
@@ -1,15 +1,15 @@
 import { NEWSLETTER_DATA_DIRECTORY } from '@lib/staticPaths';
 import { translateDateAsYearMonth } from '@scdp/ui/lib';
 
-import fsPromises from 'fs/promises';
+import fs from 'fs';
 import path from 'path';
 
 const MAX_RESULTS = 12;
 
 export const getNewsletterTitle = (dateAsYearMonth: string, title?: string) => (title ? `${title} - ${dateAsYearMonth}` : dateAsYearMonth);
 
-export const getFirstXNewsletters = async () => {
-  const years = await fsPromises.readdir(NEWSLETTER_DATA_DIRECTORY);
+export const getFirstXNewsletters = () => {
+  const years = fs.readdirSync(NEWSLETTER_DATA_DIRECTORY);
 
   const newsletters = [];
 
@@ -19,11 +19,11 @@ export const getFirstXNewsletters = async () => {
   for (let i = 0; i < years.length; i++) {
     const year = years[i];
     const yearPath = path.resolve(NEWSLETTER_DATA_DIRECTORY, `${year}`);
-    const months = await fsPromises.readdir(yearPath); //.sort().reverse();
+    const months = fs.readdirSync(yearPath); //.sort().reverse();
     months.sort().reverse();
     for (let j = 0; j < months.length; j++) {
       const month = months[j];
-      const { title, description } = JSON.parse(await fsPromises.readFile(path.resolve(yearPath, `${month}`), { encoding: 'utf-8' }));
+      const { title, description } = JSON.parse(fs.readFileSync(path.resolve(yearPath, `${month}`), { encoding: 'utf-8' }));
 
       const monthWithoutFile = month.substring(0, 2);
       newsletters.push({
@@ -45,12 +45,12 @@ export const getNewslettersByYear = async (year: string) => {
   const newsletters = [];
   // Using for loops to shortcut early
   const yearPath = path.resolve(NEWSLETTER_DATA_DIRECTORY, `${year}`);
-  const months = await fsPromises.readdir(yearPath);
+  const months = fs.readdirSync(yearPath);
   months.sort().reverse();
 
   for (let j = 0; j < months.length; j++) {
     const month = months[j];
-    const { title, description } = JSON.parse(await fsPromises.readFile(path.resolve(yearPath, `${month}`), { encoding: 'utf-8' }));
+    const { title, description } = JSON.parse(fs.readFileSync(path.resolve(yearPath, `${month}`), { encoding: 'utf-8' }));
 
     const monthWithoutFile = month.substring(0, 2);
     newsletters.push({
@@ -69,7 +69,7 @@ export const getNewslettersByYear = async (year: string) => {
 
 export const getNewsletter = async (month: string, year: string) => {
   return JSON.parse(
-    await fsPromises.readFile(path.resolve(NEWSLETTER_DATA_DIRECTORY, `${year}`, `${month}.json`), {
+    fs.readFileSync(path.resolve(NEWSLETTER_DATA_DIRECTORY, `${year}`, `${month}.json`), {
       encoding: 'utf-8',
     })
   );
