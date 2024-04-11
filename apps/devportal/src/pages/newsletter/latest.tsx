78830931aee362d2944524b644e70c4686809615
Mark van Aalst <mva@sitecore.net>
2024-04-11 15:45:37 +0200

    add ui

--- apps/devportal/src/pages/newsletter/latest.tsx
+++ apps/devportal/src/pages/newsletter/latest.tsx
@@ -1,4 +1,5 @@
 import { getLatestNewsletter } from '@/src/lib/newsletter';
+import { Box, Button, Center, Heading, Image, Link, Stack, Text } from '@chakra-ui/react';
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 
@@ -21,5 +22,22 @@ export default function Latest({ page }: { page: string }) {
     }
   }, [page, router]);
 
-  return null;
+  return (
+    <Box height={'calc(100vh - 200px)'}>
+      <Center layerStyle="section.main" h="full">
+        <Stack align="center" textAlign="center" spacing="6" maxW="sm">
+          <Image boxSize="32" src="https://sitecorecontenthub.stylelabs.cloud/api/public/content/d5bf9fd27c404416953020f5f832aaa5" alt="map-search" />
+          <Stack>
+            <Heading as="h1">Newsletter</Heading>
+            <Text variant="small">You should be redirected to the latest newsletter automatically</Text>
+          </Stack>
+          <Stack>
+            <Button variant="link">
+              <Link href={page}>Click here if the redirect doesn't work</Link>
+            </Button>
+          </Stack>
+        </Stack>
+      </Center>
+    </Box>
+  );
 }
