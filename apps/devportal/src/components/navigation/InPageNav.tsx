58ed4c41d658acea35490924f4b87454b9eed8f6
Mark van Aalst <mva@sitecore.net>
2024-04-18 16:37:15 +0200

    intial setup of IntersectionObserver

--- apps/devportal/src/components/navigation/InPageNav.tsx
+++ apps/devportal/src/components/navigation/InPageNav.tsx
@@ -1,9 +1,8 @@
-// Lib
-
 import { Heading, List, ListIcon, ListItem, Wrap } from '@chakra-ui/react';
 import { mdiCircleSmall } from '@mdi/js';
 import Link from 'next/link';
 import { title } from 'process';
+import { useEffect, useState } from 'react';
 import { ContentHeading } from '../../lib/interfaces/contentheading';
 
 type InPageNavProps = {
@@ -11,10 +10,39 @@ type InPageNavProps = {
 };
 
 const InPageNav = ({ titles }: InPageNavProps): JSX.Element => {
-  const links = titles.map((text) => ({
-    text: text.title,
-    href: `#${text.id}`,
-  }));
+  const [links, setLinks] = useState(
+    titles.map((text) => ({
+      text: text.title,
+      href: `#${text.id}`,
+      active: false,
+    }))
+  );
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            const id = entry.target.getAttribute('id');
+            setLinks((prevLinks) => prevLinks.map((link) => (link.href === `#${id}` ? { ...link, active: true } : { ...link, active: false })));
+          }
+        });
+      },
+      { threshold: 1, rootMargin: '0px 0px -90% 0px' }
+    );
+
+    titles.forEach((title) => {
+      const element = document.getElementById(title.id);
+      if (element) observer.observe(element);
+    });
+
+    return () => {
+      titles.forEach((title) => {
+        const element = document.getElementById(title.id);
+        if (element) observer.unobserve(element);
+      });
+    };
+  }, [titles]);
 
   return (
     <Wrap as={'nav'} direction="column" mt={{ base: 0, md: 10 }} mr={4} p={{ base: 2, md: 0 }} width={'2xs'} hideBelow={'xl'}>
@@ -26,7 +54,7 @@ const InPageNav = ({ titles }: InPageNavProps): JSX.Element => {
       <List spacing="1">
         {links.map((link, i) => {
           return (
-            <ListItem display={'flex'} paddingLeft={0} key={i}>
+            <ListItem display={'flex'} paddingLeft={0} key={i} fontWeight={link.active ? 'bold' : 'normal'}>
               <ListIcon boxSize={6} mr={0}>
                 <path d={mdiCircleSmall} />
               </ListIcon>
