28b58333c36f7175b068c2fb43712cc5bf649259
Mark van Aalst <mva@sitecore.net>
2024-05-13 23:20:43 +0200

    add matcher config

--- apps/devportal/src/middleware.ts
+++ apps/devportal/src/middleware.ts
@@ -3,6 +3,19 @@ import { mediaHandler } from './middlewares/mediaHandler';
 import { stackMiddlewares } from './middlewares/stackHandler';
 import { underscore } from './middlewares/underscore';
 
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    '/((?!api|_next/static|_next/image|favicon.ico).*)',
+  ],
+};
+
 const middlewares = [aspxExtension, underscore, mediaHandler];
 
 export default stackMiddlewares(middlewares);
