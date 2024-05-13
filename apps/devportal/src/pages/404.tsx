dd07d2761bd48f472428d936f64367e186c75f03
Mark van Aalst <mva@sitecore.net>
2024-05-13 15:48:05 +0200

    404 and 500 error pages

--- apps/devportal/src/pages/404.tsx
+++ apps/devportal/src/pages/404.tsx
@@ -1,13 +1,35 @@
-import { HStack, Heading, Stack } from '@chakra-ui/react';
-import {Hero, CenteredContent, VerticalGroup } from '@scdp/ui/components';
+import { Box, Center, HStack, Heading, Image, Stack, Text } from '@chakra-ui/react';
+import { ButtonLink } from '@scdp/ui/components';
 import SearchInput from '../components/sitecore-search/SearchInput';
 import Layout from '../layouts/Layout';
 
 export default function Custom404() {
   return (
     <Layout title={'This page is not available'} description={'The link you have followed might be broken, or the page has been removed'}>
-      <Hero title={'This page is not available'} description={'The link you have followed might be broken, or the page has been removed'} />
-
+      <Box height={'calc(100vh - 215px)'}>
+        <Center layerStyle="section.main" h="full">
+          <Stack align="center" textAlign="center" spacing="6" maxW="lg">
+            <Image boxSize="32" src="https://sitecorecontenthub.stylelabs.cloud/api/public/content/d5bf9fd27c404416953020f5f832aaa5" alt="map-search" />
+            <Stack>
+              <Heading as="h1">This page is not available</Heading>
+              <Text variant="small">Error 404</Text>
+            </Stack>
+            <Stack maxW={'lg'}>
+              <Heading variant="section" mb="0">
+                Maybe you can search to find what you are looking for?
+              </Heading>
+              <HStack>
+                <SearchInput showButton />
+              </HStack>
+            </Stack>
+            <Text>The page you are looking for cannot be found.</Text>
+            <Stack>
+              <ButtonLink rightIcon={undefined} variant="link" href="/" text="Go to homepage" />
+            </Stack>
+          </Stack>
+        </Center>
+      </Box>
+      {/* 
       <VerticalGroup height={'calc(100vh - 478px)'}>
         <CenteredContent>
           <Stack maxW={'lg'}>
@@ -19,7 +41,7 @@ export default function Custom404() {
             </HStack>
           </Stack>
         </CenteredContent>
-      </VerticalGroup>
+      </VerticalGroup>*/}
     </Layout>
   );
 }
---
+++ apps/devportal/src/pages/500.tsx
@@ -0,0 +1,38 @@
+import { Box, Center, Heading, Image, Stack, Text } from '@chakra-ui/react';
+import { ButtonLink } from '@scdp/ui/components';
+import Layout from '../layouts/Layout';
+
+export default function Custom500() {
+  return (
+    <Layout title={'Internal Server error'} description={'The link you have followed might be broken, or the page has been removed'}>
+      <Box height={'calc(100vh - 215px)'}>
+        <Center layerStyle="section.main" h="full">
+          <Stack align="center" textAlign="center" spacing="6" maxW="lg">
+            <Image boxSize="32" src="https://sitecorecontenthub.stylelabs.cloud/api/public/content/4e1101727f1a48aab23deba3947b6a9c?" alt="alert" />
+            <Stack>
+              <Heading as="h1">Internal server error</Heading>
+              <Text variant="small">Error 500</Text>
+            </Stack>
+            <Text>Apologies, this page seems to be broken.</Text>
+            <Stack>
+              <ButtonLink rightIcon={undefined} variant="link" href="/" text="Go to homepage" />
+            </Stack>
+          </Stack>
+        </Center>
+      </Box>
+      {/* 
+      <VerticalGroup height={'calc(100vh - 478px)'}>
+        <CenteredContent>
+          <Stack maxW={'lg'}>
+            <Heading size="md" mb="0">
+              Maybe you can search to find what you are looking for?
+            </Heading>
+            <HStack>
+              <SearchInput showButton />
+            </HStack>
+          </Stack>
+        </CenteredContent>
+      </VerticalGroup>*/}
+    </Layout>
+  );
+}
