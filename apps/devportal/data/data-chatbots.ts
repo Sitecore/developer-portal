64a9af340bdcb32257fd7936e8098fdfb496fff4
Rob Earlam <robert.earlam@gmail.com>
2024-05-16 11:33:43 +1000

    Added more chatbot profiles

--- apps/devportal/data/data-chatbots.ts
+++ apps/devportal/data/data-chatbots.ts
@@ -16,5 +16,20 @@ export const AllChatbots: ChatBotType[] = [
     name: "Rob",
     image: "/images/chatbots/Rob.png",
     talkingImage: "/images/chatbots/Rob-talking.gif"
+  },
+  {
+    name: "Andy",
+    image: "/images/chatbots/AndyC.png",
+    talkingImage: "/images/chatbots/AndyC-talking.gif"
+  },
+  {
+    name: "Pieter",
+    image: "/images/chatbots/Pieter.png",
+    talkingImage: "/images/chatbots/Pieter-talking.gif"
+  },
+  {
+    name: "Roger",
+    image: "/images/chatbots/Roger.png",
+    talkingImage: "/images/chatbots/Roger-talking.gif"
   }
 ]
