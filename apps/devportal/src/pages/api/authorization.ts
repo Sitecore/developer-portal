3925c4fb4f2cdb8f163d00a61304d6739a1ba8cf
Mark van Aalst <mva@sitecore.net>
2024-05-16 13:53:38 +0200

    change to 204 status code (current)

--- apps/devportal/src/pages/api/authorization.ts
+++ apps/devportal/src/pages/api/authorization.ts
@@ -1,8 +1,13 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  res.statusCode = 299;
-  res.setHeader('Content-Type', 'application/json');
-  res.setHeader('Warning: 299', 'Deprecated API');
-  res.end(JSON.stringify({ message: 'Deprecated endpoint' }));
+
+  if (req.method === 'POST') {
+    res.statusCode = 204;
+    res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Warning', '299 - Deprecated API');
+    res.end(JSON.stringify({ message: 'Deprecated endpoint' }));
+  }
+  res.statusCode = 204;
+  res.end();
 };
