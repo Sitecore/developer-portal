cb34d4296ada212036f6a2ef207fd8aba6a9ee97
Mark van Aalst <mva@sitecore.net>
2024-05-17 15:42:13 +0200

    add first draft of oss page

---
+++ apps/devportal/src/components/repositories.tsx
@@ -0,0 +1,31 @@
+import { Alert, AlertDescription, AlertIcon, AlertTitle, Heading, Link, SimpleGrid } from '@chakra-ui/react';
+import { Repository } from '@scdp/ui/components';
+import { GitHubRepo } from '../lib/interfaces/github';
+
+export type RepositoryProps = {
+  repositories: GitHubRepo[];
+};
+
+const Repositories = ({ repositories }: RepositoryProps) => {
+  return (
+    <>
+      <Heading>Recently updated repositories</Heading>
+      <SimpleGrid columns={3} spacing={10}>
+        {repositories.length > 0 &&
+          repositories.map((repo) => (
+            <Repository key={repo.url} name={repo.full_name} description={repo.description} repositoryUrl={repo.html_url} framework={repo.language} forks={repo.forks_count} stars={repo.stargazers_count} topics={repo.topics} />
+          ))}
+      </SimpleGrid>
+
+      <Alert>
+        <AlertIcon />
+        <AlertTitle>Looking for more open source projects?</AlertTitle>
+        <AlertDescription>
+          Look for the <Link href="https://github.com/orgs/Sitecore/repositories">Sitecore organization</Link> on GitHub
+        </AlertDescription>
+      </Alert>
+    </>
+  );
+};
+
+export default Repositories;
---
+++ apps/devportal/src/pages/opensource.tsx
@@ -0,0 +1,46 @@
+import { PageInfo } from '@lib/interfaces/page-info';
+import { getPageInfo } from '@lib/page-info';
+import { CenteredContent, Hero, VerticalGroup } from '@scdp/ui/components';
+import { NextPage } from 'next';
+import { TrackPageView } from '../components/engagetracker/TrackPageView';
+import { MarkDownContent } from '../components/markdown/MarkdownContent';
+import Repositories from '../components/repositories';
+import Layout from '../layouts/Layout';
+import { getGitHubRepositories } from '../lib/github';
+import { GitHubRepo } from '../lib/interfaces/github';
+
+interface OpenSourceProps {
+  pageInfo: PageInfo;
+  repositories: GitHubRepo[];
+}
+
+export async function getServerSideProps() {
+  const pageInfo = await getPageInfo('opensource');
+  const repositories = await getGitHubRepositories(9);
+
+  return {
+    props: {
+      pageInfo,
+      repositories,
+    },
+  };
+}
+
+const OpenSource: NextPage<OpenSourceProps> = ({ pageInfo, repositories }) => {
+  return (
+    <TrackPageView pageInfo={pageInfo}>
+      <Layout title={pageInfo.title} description={pageInfo.description} openGraphImage={pageInfo.openGraphImage}>
+        <Hero title={pageInfo.title} description={pageInfo.description} image={pageInfo.heroImage} productLogo={pageInfo.productLogo} />
+
+        <VerticalGroup>
+          <CenteredContent>
+            <MarkDownContent content={pageInfo.parsedContent} />
+            <Repositories repositories={repositories} />
+          </CenteredContent>
+        </VerticalGroup>
+      </Layout>
+    </TrackPageView>
+  );
+};
+
+export default OpenSource;
