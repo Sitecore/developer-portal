f234d99f821a63c55a56d38bf434bcbd45acd450
Mark van Aalst <mva@sitecore.net>
2024-06-24 17:21:46 +0200

    migrate dam images

--- apps/devportal/data/promos/newsletter.ts
+++ apps/devportal/data/promos/newsletter.ts
@@ -5,7 +5,7 @@ const data: PromoCardProps = {
   description:
     'Sitecore for Developers newsletter bundles up the best technical info available from our global developer ecosystem and delivers it directly to your inbox. Each month, Sitecore for Developers will bring you: Product announcements, new features, and improvements.',
   img: {
-    src: 'https://mss-p-006-delivery.stylelabs.cloud/api/public/content/2bc771703b30475a822c08c61493cee8?v=199716f6',
+    src: 'https://delivery-sitecore.sitecorecontenthub.cloud/api/public/content/c8149349562d4aa6884ff4243c1ed60e?v=b77361d3',
   },
   link: {
     href: 'https://www.sitecore.com/newsletter/sitecore-for-developers',
