ccf169f78e5dcc7e330ee8ffbf29cbdd98b44c39
Mark van Aalst <markvanaalst@gmail.com>
2024-07-25 23:51:52 +0200

    add playwright

--- apps/devportal/src/lib/page-info.ts
+++ apps/devportal/src/lib/page-info.ts
@@ -10,6 +10,7 @@ import { Changelog } from '@scdp/changelog';
 
 import { SITECORE_COMMUNITY_MAX_COUNT, SitecoreCommunityApi, SitecoreCommunityContent, SitecoreCommunityEvent, StackExchangeApi, YouTubeApi } from '../components/integrations';
 import { getChangelogCredentials } from './changelog/changelog';
+import { searchForFile } from './utils/fsUtils';
 
 const dataDirectory = path.join(process.cwd(), 'data/markdown');
 const pagesDirectory = path.join(dataDirectory, 'pages');
@@ -202,29 +203,3 @@ export const getChildNavgationInfo = async (currentUrlSegment: string): Promise<
     return fileData;
   }
 };
-
-function searchForFile(folderPath: string, fileName: string): string | null {
-  const filePath = path.join(folderPath, fileName);
-
-  try {
-    // Check if the file exists in the current folder
-    if (fs.existsSync(filePath) && fs.statSync(filePath).isFile()) {
-      return filePath;
-    } else {
-      // If not found, check the parent folder
-      const parentFolder = path.dirname(folderPath);
-
-      // Base case: If we've reached the root folder and still haven't found the file, return null
-      if (parentFolder === folderPath) {
-        return null;
-      }
-
-      // Recursively search in the parent folder
-      return searchForFile(parentFolder, fileName);
-    }
-  } catch (err) {
-    // Handle any errors that occur during the search
-    console.error(`Error searching for file ${fileName}: ${err}`);
-    return null;
-  }
-}
