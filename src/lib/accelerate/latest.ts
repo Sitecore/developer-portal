72697fdfe3d0b5ac65449dbd5cc6e2a5922731c3
Mark van Aalst <mva@sitecore.net>
2024-09-19 16:21:43 +0200

    add rss feed

---
+++ src/lib/accelerate/latest.ts
@@ -0,0 +1,45 @@
+import fs from 'fs';
+import matter from 'gray-matter';
+import path from 'path';
+import { convertFileToURL, getAllMdFiles } from '../utils/fsUtils';
+import { AccelerateRecipe } from './types/recipe';
+
+export const getLatestRecipes = async (product: string) => {
+  const dataDirectory = path.join(process.cwd(), 'data/markdown/pages');
+  const recipesRoot = path.join(dataDirectory, 'learn', 'accelerate', product);
+
+  if (!fs.existsSync(recipesRoot)) {
+    return null;
+  }
+
+  const recipeFiles = getAllMdFiles(recipesRoot);
+
+  const recipes = await Promise.all(
+    recipeFiles.map(async (file) => {
+      const content = await fs.promises.readFile(file, 'utf-8');
+      const { data } = matter(content);
+      if (data.lastUpdated) {
+        return {
+          lastUpdated: data.lastUpdated,
+          title: data.title,
+          description: data.description,
+          url: convertFileToURL(file),
+        };
+      }
+      return null;
+    })
+  );
+
+  const filteredRecipes = recipes
+    .filter((recipe) => recipe !== null)
+    .sort((a, b) => {
+      if (a.lastUpdated < b.lastUpdated) {
+        return 1;
+      } else if (a.lastUpdated > b.lastUpdated) {
+        return -1;
+      } else {
+        return 0;
+      }
+    }) as Array<AccelerateRecipe>;
+  return filteredRecipes;
+};
