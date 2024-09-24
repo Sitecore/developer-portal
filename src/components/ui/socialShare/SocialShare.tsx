a7067604048e9cb856b1ac03b1bd92940051c946
Mark van Aalst <mva@sitecore.net>
2024-09-24 13:54:00 +0200

    remove popup

--- src/components/ui/socialShare/SocialShare.tsx
+++ src/components/ui/socialShare/SocialShare.tsx
@@ -1,5 +1,5 @@
-import { Box, HStack, Icon, IconButton, Popover, PopoverArrow, PopoverBody, PopoverContent, PopoverTrigger, Tooltip, useClipboard, useToast } from '@chakra-ui/react';
-import { mdiCheck, mdiChevronUp, mdiContentCopy } from '@mdi/js';
+import { Box, Collapse, HStack, Icon, IconButton, Tooltip, useClipboard, useDisclosure, useToast } from '@chakra-ui/react';
+import { mdiCheck, mdiChevronLeft, mdiChevronRight, mdiContentCopy } from '@mdi/js';
 import { EmailIcon, EmailShareButton, LinkedinIcon, LinkedinShareButton, RedditIcon, RedditShareButton, TelegramIcon, TelegramShareButton, TwitterIcon, TwitterShareButton, WhatsappIcon, WhatsappShareButton } from 'next-share';
 
 type SocialShareProps = {
@@ -9,49 +9,46 @@ type SocialShareProps = {
 
 export const SocialShare = ({ title, url }: SocialShareProps): JSX.Element => {
   const { onCopy, hasCopied } = useClipboard(url);
+  const { isOpen, onToggle } = useDisclosure();
   const toast = useToast();
 
   return (
-    <HStack gap={1}>
-      <Popover placement="top" closeOnBlur closeOnEsc>
-        <PopoverTrigger>
-          <IconButton
-            variant="ghost"
-            aria-label="More sharing methods"
-            icon={
-              <Icon boxSize={5}>
-                <path d={mdiChevronUp} />
-              </Icon>
-            }
-            boxSize={8}
-            minWidth={4}
-            mx={1}
-          />
-        </PopoverTrigger>
+    <HStack gap={0}>
+      <Tooltip label="More sharing methods" aria-label="More sharing methods">
+        <IconButton
+          variant="ghost"
+          aria-label="More sharing methods"
+          onClick={onToggle}
+          icon={
+            <Icon>
+              <path d={isOpen ? mdiChevronRight : mdiChevronLeft} />
+            </Icon>
+          }
+          boxSize={8}
+          minWidth={4}
+          mx={1}
+        />
+      </Tooltip>
 
-        <PopoverContent style={{ maxWidth: 'unset', width: 'unset' }} shadow={'lg'}>
-          <PopoverArrow />
-          <PopoverBody p={4}>
-            <HStack gap={4}>
-              <WhatsappShareButton url={url} title={title}>
-                <Tooltip label="Share link by Whatsapp" aria-label="Share link by Whatsapp">
-                  <IconButton variant="ghost" aria-label="Share by Whatsapp" icon={<WhatsappIcon size={32} round />} as={Box} />
-                </Tooltip>
-              </WhatsappShareButton>
-              <TelegramShareButton url={url} title={title}>
-                <Tooltip label="Share link by Telegram" aria-label="Share link by Telegram">
-                  <IconButton variant="ghost" aria-label="Share by Telegram" icon={<TelegramIcon size={32} round />} as={Box} />
-                </Tooltip>
-              </TelegramShareButton>
-              <RedditShareButton url={url} title={title}>
-                <Tooltip label="Share link by Reddit" aria-label="Share link by Reddit">
-                  <IconButton variant="ghost" aria-label="Share by Reddit" icon={<RedditIcon size={32} round />} as={Box} />
-                </Tooltip>
-              </RedditShareButton>
-            </HStack>
-          </PopoverBody>
-        </PopoverContent>
-      </Popover>
+      <Collapse in={isOpen} animateOpacity>
+        <HStack hidden={!isOpen} gap={0}>
+          <WhatsappShareButton url={url} title={title}>
+            <Tooltip label="Share link by Whatsapp" aria-label="Share link by Whatsapp">
+              <IconButton variant="ghost" aria-label="Share by Whatsapp" icon={<WhatsappIcon size={32} round />} as={Box} />
+            </Tooltip>
+          </WhatsappShareButton>
+          <TelegramShareButton url={url} title={title}>
+            <Tooltip label="Share link by Telegram" aria-label="Share link by Telegram">
+              <IconButton variant="ghost" aria-label="Share by Telegram" icon={<TelegramIcon size={32} round />} as={Box} />
+            </Tooltip>
+          </TelegramShareButton>
+          <RedditShareButton url={url} title={title}>
+            <Tooltip label="Share link by Reddit" aria-label="Share link by Reddit">
+              <IconButton variant="ghost" aria-label="Share by Reddit" icon={<RedditIcon size={32} round />} as={Box} />
+            </Tooltip>
+          </RedditShareButton>
+        </HStack>
+      </Collapse>
 
       <EmailShareButton url={url} title={title}>
         <Tooltip label="Share link by email" aria-label="Share link by email">
