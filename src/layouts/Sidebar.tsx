cd0f69731f0f02553a96c6a23da7c575d16f892a
Mark van Aalst <mva@sitecore.net>
2024-11-28 16:41:53 +0100

    show sidebar on small screens

--- src/layouts/Sidebar.tsx
+++ src/layouts/Sidebar.tsx
@@ -7,7 +7,7 @@ interface SidebarProps extends BoxProps {
 
 export const Sidebar = ({ children, showBackground = false, ...rest }: SidebarProps): JSX.Element => {
   return (
-    <Box as={'aside'} w={{ base: 'full', md: 'sm', sm: 'full' }} order={{ base: -1, md: 0 }} layerStyle={showBackground && children ? 'section.sidebar' : rest.layerStyle} hideBelow={'xl'}>
+    <Box as={'aside'} w={{ base: 'full', md: 'sm', sm: 'full' }} order={{ base: -1, md: 0 }} layerStyle={showBackground && children ? 'section.sidebar' : rest.layerStyle}>
       {children && (
         <Wrap direction="column" h={showBackground ? { base: 'auto', md: `auto` } : {}} top="50" shadow={'none'} display="flex" flexFlow="column nowrap" as={'div'} {...rest}>
           {children}
