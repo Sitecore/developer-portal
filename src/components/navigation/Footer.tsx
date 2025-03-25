cd6af6630a067a439b86767baa210650ab537303
Mark van Aalst <mva@sitecore.net>
2025-03-25 16:40:39 +0100

    Refactor Footer and ScrollToTop components for improved layout and responsiveness. Update Alert position in AcceleratePage for better visibility.

--- src/components/navigation/Footer.tsx
+++ src/components/navigation/Footer.tsx
@@ -1,4 +1,4 @@
-import { Box, Container, HStack, Icon, Link, Stack, Text, Tooltip } from '@chakra-ui/react';
+import { Flex, HStack, Icon, Link, Stack, StackDivider, Text, Tooltip } from '@chakra-ui/react';
 import NextLink from 'next/link';
 import { FaFacebook, FaInstagram, FaLinkedin, FaTwitter, FaYoutube } from 'react-icons/fa';
 
@@ -6,39 +6,38 @@ import { mdiStarFourPoints } from '@mdi/js';
 import { SocialButton } from '../links';
 
 export const Footer = () => (
-  <Box bg={'chakra-subtle-bg'} borderTop={'1px'} borderColor={'chakra-border-color'} position={'relative'} bottom={0} left={0} width={'full'}>
-    <Container as={HStack} maxW={'7xl'} py={8} px={2} direction={{ base: 'column', md: 'column' }} spacing={4} gap={8} width={'full'} justifyContent={'space-between'}>
-      <HStack>
+  <Flex py={8} px={2} direction={{ base: 'column-reverse', md: 'row' }} gap={4} width={'full'} bg={'chakra-subtle-bg'} borderTop={'1px'} borderColor={'chakra-border-color'}>
+    <Flex direction={{ base: 'column', md: 'row' }} width={'full'} justifyContent={'space-between'} maxWidth={'7xl'} mx={'auto'}>
+      <Stack direction={{ base: 'column', md: 'row' }} spacing={6} alignItems={'center'}>
         <Text>© Copyright 2025, Sitecore. All Rights Reserved</Text>
-        <Text display={{ base: 'none', md: 'block' }}>|</Text>
-        <Link as={NextLink} href={'https://www.sitecore.com/legal'} target="_blank" title="Opens in new tab">
-          <Text transition={'all .3s ease'} fontWeight={500} fontSize={'sm'}>
-            Legal
-          </Text>
-        </Link>
-        <Text>|</Text>
-        <Link as={NextLink} href={'https://www.sitecore.com/trust/privacy-policy'} target="_blank" title="Opens in new tab">
-          <Text transition={'all .3s ease'} fontWeight={500} fontSize={'sm'}>
-            Privacy
-          </Text>
-        </Link>
-        <Text>|</Text>
-        <Link as={NextLink} href={'/help'} target="_blank" title="Opens in new tab">
-          <Text transition={'all .3s ease'} fontWeight={500} fontSize={'sm'}>
-            Get Help
-          </Text>
-        </Link>
-        <Text>|</Text>
-        <Tooltip label="Integrate Sitecore resources using LLMs" placement="top">
-          <Link href={'/llms.txt'} title="Read LLM file">
+
+        <HStack divider={<StackDivider borderColor="gray.200" />} mb={{ base: 4, md: 0 }}>
+          <Link as={NextLink} href={'https://www.sitecore.com/legal'} target="_blank" title="Opens in new tab">
             <Text transition={'all .3s ease'} fontWeight={500} fontSize={'sm'}>
-              LLM
+              Legal
             </Text>
           </Link>
-        </Tooltip>
-      </HStack>
+          <Link as={NextLink} href={'https://www.sitecore.com/trust/privacy-policy'} target="_blank" title="Opens in new tab">
+            <Text transition={'all .3s ease'} fontWeight={500} fontSize={'sm'}>
+              Privacy
+            </Text>
+          </Link>
+          <Link as={NextLink} href={'/help'} target="_blank" title="Opens in new tab">
+            <Text transition={'all .3s ease'} fontWeight={500} fontSize={'sm'}>
+              Get Help
+            </Text>
+          </Link>
+          <Tooltip label="Integrate Sitecore resources using LLMs" placement="top">
+            <Link href={'/llms.txt'} title="Read LLM file">
+              <Text transition={'all .3s ease'} fontWeight={500} fontSize={'sm'}>
+                LLM
+              </Text>
+            </Link>
+          </Tooltip>
+        </HStack>
+      </Stack>
 
-      <Stack direction={'row'} spacing={6}>
+      <Stack direction={'row'} spacing={6} justifyContent={'center'}>
         <SocialButton label={'Twitter'} href={'https://twitter.com/Sitecore'} aria-label="Twitter">
           <FaTwitter />
         </SocialButton>
@@ -60,6 +59,6 @@ export const Footer = () => (
           </Icon>
         </SocialButton>
       </Stack>
-    </Container>
-  </Box>
+    </Flex>
+  </Flex>
 );
