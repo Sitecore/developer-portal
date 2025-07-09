488d665c67ad6d7ad3c418b99000949922ac8f18
Mark van Aalst <mva@sitecore.net>
2025-07-09 16:47:35 +0200

    Update Login component to conditionally display sign-in options based on redirect URL, enhancing user experience by hiding unnecessary options for download paths.

--- src/components/authentication/Login.tsx
+++ src/components/authentication/Login.tsx
@@ -13,13 +13,17 @@ export const Login = ({ redirectUrl = '/' }: LoginProps) => {
         <Heading as="h1">Login</Heading>
       </Stack>
       <Stack maxW={'lg'}>
-        <Heading variant="section" mb="0" fontSize="xl">
-          Choose your preferred sign-in method
-        </Heading>
+        {!redirectUrl.startsWith('/downloads') && (
+          <Heading variant="section" mb="0" fontSize="xl">
+            Choose your preferred sign-in method
+          </Heading>
+        )}
       </Stack>
       <Card variant={'elevated'}>
         <Stack py={10} px={4}>
-          <Text variant="small">To access the downloads, please sign in using your Sitecore ID</Text>
+          <Text variant="small" mb={8}>
+            To access the downloads, please sign in using your Sitecore ID
+          </Text>
           <Wrap justify="center">
             <Tooltip label="Sign in using your Sitecore ID to get access to the downloads">
               <Button
@@ -35,28 +39,32 @@ export const Login = ({ redirectUrl = '/' }: LoginProps) => {
               </Button>
             </Tooltip>
           </Wrap>
-          <Box position="relative" py={10} px={4}>
-            <Divider />
-            <AbsoluteCenter bg="chakra-body-bg" px="4" color="neutral.400">
-              OR
-            </AbsoluteCenter>
-          </Box>
-          <Text variant="small">To access the roadmap, please sign in using your Sitecore Cloud Portal account</Text>
-          <Wrap justify="center" align="center">
-            <Tooltip label="Sign in using your Sitecore ID to get access to the roadmap">
-              <Button
-                variant="outline"
-                leftIcon={
-                  <Icon boxSize="icon.md" color="red">
-                    <path d={iconSitecore} />
-                  </Icon>
-                }
-                onClick={() => signIn('sitecore', { callbackUrl: redirectUrl })}
-              >
-                Sitecore Cloud Portal
-              </Button>
-            </Tooltip>
-          </Wrap>
+          {!redirectUrl.startsWith('/downloads') && (
+            <>
+              <Box position="relative" py={10} px={4}>
+                <Divider />
+                <AbsoluteCenter bg="chakra-body-bg" px="4" color="neutral.400">
+                  OR
+                </AbsoluteCenter>
+              </Box>
+              <Text variant="small">To access the roadmap, please sign in using your Sitecore Cloud Portal account</Text>
+              <Wrap justify="center" align="center">
+                <Tooltip label="Sign in using your Sitecore ID to get access to the roadmap">
+                  <Button
+                    variant="outline"
+                    leftIcon={
+                      <Icon boxSize="icon.md" color="red">
+                        <path d={iconSitecore} />
+                      </Icon>
+                    }
+                    onClick={() => signIn('sitecore', { callbackUrl: redirectUrl })}
+                  >
+                    Sitecore Cloud Portal
+                  </Button>
+                </Tooltip>
+              </Wrap>
+            </>
+          )}
         </Stack>
       </Card>
     </Stack>
