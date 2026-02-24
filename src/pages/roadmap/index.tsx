3c44a0c1fdf8af6cd23cb7e560f91fb8e1f498f4
Mark van Aalst <mva@sitecore.net>
2026-02-24 21:19:32 +0100

    chore: update package-lock.json and simplify roadmap page by removing unused imports and components

--- src/pages/roadmap/index.tsx
+++ src/pages/roadmap/index.tsx
@@ -1,12 +1,6 @@
-import { Item, ItemActions, ItemContent } from '@/src/components/ui/item';
-import { slugify } from '@/src/lib/util/stringUtil';
-import { mdiArrowRight } from '@mdi/js';
-import Icon from '@mdi/react';
-import { HideForUsers } from '@src/components/authentication/HideForUsers';
 import { RestrictedContent } from '@src/components/authentication/RestrictedContent';
 import { TrackPageView } from '@src/components/integrations/engage/TrackPageView';
 import { Alert, AlertDescription, AlertTitle } from '@src/components/ui/alert';
-import { Button } from '@src/components/ui/button';
 import type { Option } from '@src/components/ui/dropdown';
 import { CenteredContent, Hero, VerticalGroup } from '@src/components/ui/sections';
 import Layout from '@src/layouts/Layout';
@@ -15,9 +9,6 @@ import type { PageInfo } from '@src/lib/interfaces/page-info';
 import { getRoadmap } from '@src/lib/jira';
 import { getPageInfo } from '@src/lib/page-info';
 import type { NextPage } from 'next';
-import { signIn } from 'next-auth/react';
-import Image from 'next/image';
-import Link from 'next/link';
 
 interface RoadmapPageProps {
   pageInfo: PageInfo;
@@ -27,16 +18,11 @@ interface RoadmapPageProps {
 
 export async function getServerSideProps() {
   const pageInfo = await getPageInfo('_roadmap');
-  const roadmap = await getRoadmap();
 
-  return { props: { pageInfo, products: roadmap.products } };
+  return { props: { pageInfo } };
 }
 
-function fixTitle(product: string) {
-  return product.replace('Ai', 'AI');
-}
-
-const Roadmap: NextPage<RoadmapPageProps> = ({ pageInfo, products }) => {
+const Roadmap: NextPage<RoadmapPageProps> = ({ pageInfo}) => {
   return (
     <TrackPageView pageInfo={pageInfo}>
       <Layout title={pageInfo.title} description={pageInfo.description} openGraphImage={pageInfo.openGraphImage}>
