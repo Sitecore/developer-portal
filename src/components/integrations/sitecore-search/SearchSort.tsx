f28ae96de02d5588eae592f37a9748201d49ffc0
Mark van Aalst <mark.vanaalst@sitecore.com>
2026-05-13 23:30:32 +0200

    fix search rendering

--- src/components/integrations/sitecore-search/SearchSort.tsx
+++ src/components/integrations/sitecore-search/SearchSort.tsx
@@ -1,14 +1,11 @@
 /* eslint-disable no-unused-vars */
 
-import type { SearchResponseSortChoice } from "@sitecore-search/react";
-import { Button } from "@src/components/ui/button";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@src/components/ui/dropdown-menu";
-import { Check, ChevronDown } from "lucide-react";
+import { Icon } from '@/lib/icon';
+import { mdiChevronDown } from '@mdi/js';
+import type { SearchResponseSortChoice } from '@sitecore-search/react';
+import { Button } from '@src/components/ui/button';
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@src/components/ui/dropdown-menu';
+import { Check } from 'lucide-react';
 
 export interface SearchSortType {
   onSortChange: (sortType: any) => void;
@@ -22,17 +19,14 @@ export const SearchSort = (props: SearchSortType) => {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline">
-          Sorting by:
-          <ChevronDown className="ml-2 h-4 w-4" />
+        <Button variant="outline" size="sm">
+          Sort by:
+          <Icon path={mdiChevronDown} size={1.3} className="opacity-50" />
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent>
         {sortChoices.map((option) => (
-          <DropdownMenuItem
-            key={option.name}
-            onClick={() => onSortChange(option)}
-          >
+          <DropdownMenuItem key={option.name} onClick={() => onSortChange(option)}>
             {sortType === option.name && <Check className="mr-2 h-4 w-4" />}
             {option.label}
           </DropdownMenuItem>
